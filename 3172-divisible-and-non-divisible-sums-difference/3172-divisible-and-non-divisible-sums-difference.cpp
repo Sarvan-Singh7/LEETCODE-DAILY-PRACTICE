d6c09a31ef2b56b757121342cdeffac1df65ad15
SARVAN SINGH <thakurshravan164@gmail.com>
2025-08-31 16:18:15 +0530

    Time: 0 ms (100%), Space: 8.5 MB (33.82%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        vector<int>divisible; //creating a vector divisible which store numbers that are divisible  by m
+        
         int num1=0,num2=0;
-        vector<int>notDiv;   //store those which are not divisible
+        
         for(int i=1;i<=n;i++){
                 if(i%m==0){
-                    divisible.push_back(i);
                     num1+=i;      //calculating sum of elements which are present in divisible;
                 }
                 else{
-                    notDiv.push_back(i);
                     num2+=i;     //calculating sum which are not divisible
                 }
         }
