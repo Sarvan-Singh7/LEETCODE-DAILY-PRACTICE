d5ad8c1cca8cf6f2c57f8424140ac4263eaeb0c7
SARVAN SINGH <thakurshravan164@gmail.com>
2025-09-03 16:31:28 +0530

    Time: 3 ms (7.46%), Space: 11.3 MB (80.8%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int n=heights.size();
+        vector<int> expected;
+        expected=heights;
+        sort(expected.begin(),expected.end());
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(heights[i]!=expected[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
