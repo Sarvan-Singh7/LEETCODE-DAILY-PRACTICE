f0b6d529e11c3e4623598694672c857817d2ef03
SARVAN SINGH <thakurshravan164@gmail.com>
2025-09-29 14:26:57 +0530

    Time: 0 ms (100%), Space: 42.1 MB (45.24%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -8,33 +8,44 @@
 //     }
 // }
 
-
-
-
-
-class Solution {
-    public int findMin(int[] arr) {
-        int n= arr.length;
-        int st=0;
-        int end=n-1;
-        int answer = Integer.MAX_VALUE;
-        while(st<= end){
-          int mid = st + (end  - st )/2;
-          if(arr[st] <= arr[end]){
-            answer = Math.min(arr[st] , answer);
-            break;
-          }
-
-          if(arr[mid] <= arr[end]){
-              
-              answer = Math.min(arr[mid] , answer);
-              end = mid -1;
-          }
-          else{
-            answer = Math.min(arr[st] , answer);
-            st = mid+1;
-          }
-        }
-        return answer;
+class Solution{
+  public static int findMin(int []arr) {
+        int si =0,end = arr.length -1;
+        while(si<end){
+            int mid = si +(end - si) /2;
+        if(arr[mid] > arr[end]) si = mid +1;
+        else if(arr[mid] < arr[end]) end = mid;
+        else end--;
     }
+    return arr[si];
+  }
 }
+
+
+
+// class Solution {
+//     public int findMin(int[] arr) {
+//         int n= arr.length;
+//         int st=0;
+//         int end=n-1;
+//         int answer = Integer.MAX_VALUE;
+//         while(st<= end){
+//           int mid = st + (end  - st )/2;
+//           if(arr[st] <= arr[end]){
+//             answer = Math.min(arr[st] , answer);
+//             break;
+//           }
+
+//           if(arr[mid] <= arr[end]){
+              
+//               answer = Math.min(arr[mid] , answer);
+//               end = mid -1;
+//           }
+//           else{
+//             answer = Math.min(arr[st] , answer);
+//             st = mid+1;
+//           }
+//         }
+//         return answer;
+//     }
+// }
