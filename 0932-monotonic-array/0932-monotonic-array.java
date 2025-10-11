a32f5699ad5f9c38245aa1d6f8cea057cbed8070
SARVAN SINGH <thakurshravan164@gmail.com>
2025-10-12 00:17:59 +0530

    Time: 3 ms (56.46%), Space: 59.5 MB (28.68%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int n=nums.length;
+        int inc=0,dec=0;
+        for(int i=0;i<n-1;i++){
+            if(nums[i] < nums[i+1]){
+                inc =1;
+                continue;
+            }
+            else if(nums[i]>nums[i+1]){
+                dec=1;
+                continue;
+            }
+        }
+        if(inc==1 && dec==1){
+            return false;
+        }
+        if((inc ==1 && dec==0 )|| (inc==0 && dec==1)){
+            return true;
+        }
+        return true;
+    }
+}
