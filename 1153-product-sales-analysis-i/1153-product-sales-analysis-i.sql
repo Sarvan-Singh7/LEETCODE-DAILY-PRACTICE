0cccaa7eda9634ffd26ba25b3e4a083b140e4cf1
SARVAN SINGH <thakurshravan164@gmail.com>
2025-10-22 20:27:56 +0530

    Time: 4719 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select Product.product_name , Sales.year , Sales.price 
+FROM
+Sales inner join Product 
+ON
+Sales.Product_id = Product.product_id;
+#\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af\U0001f4af
