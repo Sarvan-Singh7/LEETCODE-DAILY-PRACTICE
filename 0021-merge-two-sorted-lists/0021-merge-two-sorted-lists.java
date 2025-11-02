9103b66f111dfb7f7225f71bada4fc9b742c1b49
SARVAN SINGH <thakurshravan164@gmail.com>
2025-11-02 19:52:11 +0530

    Time: 0 ms (100%), Space: 44.2 MB (8.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,31 +1,53 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode temp1 = list1;
+//         ListNode temp2 = list2;
+//         ListNode dummy = new ListNode(0) ; //dummy node created to return
+//         ListNode temp = dummy; //dummy pe temp ko point kar diya so that make operations with temp
+
+//         while(temp1 != null && temp2 !=null){
+//             if(temp1.val <=temp2.val){
+//                 ListNode a = new ListNode(temp1.val);
+//                 temp.next = a;    //as a ko temp1 se lekar temp ke piche lagaya
+//                 temp=a;           //temp ko bhi a pe kardo so that uska ext sahi mille
+//                 temp1=temp1.next;   //for excluding a from node temp=temp.next kardo
+//             }
+//             else{
+//                 ListNode a=new ListNode(temp2.val);
+//                 temp.next =a;
+//                 temp=a;
+//                 temp2=temp2.next;
+//             }
+//         }
+//         if(temp1 ==null){
+//             temp.next = temp2;
+//         }else{
+//             temp.next = temp1;
+//         }
+//         return dummy.next;
+//     }
+// }
+
+
+////////////////////////////OPTIMAL SOLUTION AS (a) or new NODE not present
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode temp1 = list1;
         ListNode temp2 = list2;
-        ListNode dummy = new ListNode(0) ; //dummy node created to return
-        ListNode temp = dummy; //dummy pe temp ko point kar diya so that make operations with temp
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
 
-        while(temp1 != null && temp2 !=null){
-            if(temp1.val <=temp2.val){
-                ListNode a = new ListNode(temp1.val);
-                temp.next = a;    //as a ko temp1 se lekar temp ke piche lagaya
-                temp=a;           //temp ko bhi a pe kardo so that uska ext sahi mille
-                temp1=temp1.next;   //for excluding a from node temp=temp.next kardo
+        while(temp1 !=null && temp2 != null){
+            if(temp1.val <= temp2.val){
+                temp.next = temp1;
+                temp = temp1;      //ye hi difference hai that temp ko hi temp1 pe point
+                temp1=temp1.next;
             }
             else{
-                ListNode a=new ListNode(temp2.val);
-                temp.next =a;
-                temp=a;
+                temp.next = temp2;
+                temp = temp2;
                 temp2=temp2.next;
             }
         }
@@ -35,5 +57,6 @@ class Solution {
             temp.next = temp1;
         }
         return dummy.next;
+
     }
 }
