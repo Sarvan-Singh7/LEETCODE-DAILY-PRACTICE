75fc3adbba1b890c49c68b960ff40669f0d5d0b8
SARVAN SINGH <thakurshravan164@gmail.com>
2025-11-10 14:52:12 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -32,28 +32,62 @@
 
 ////////////////////////////OPTIMAL SOLUTION AS (a) or new NODE not present
 
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode temp1 = list1;
+//         ListNode temp2 = list2;
+//         ListNode dummy = new ListNode(0);
+//         ListNode temp = dummy;
+
+//         while(temp1 !=null && temp2 != null){
+//             if(temp1.val <= temp2.val){
+//                 temp.next = temp1;
+//                 temp = temp1;      //ye hi difference hai that temp ko hi temp1 pe point
+//                 temp1=temp1.next;
+//             }
+//             else{
+//                 temp.next = temp2;
+//                 temp = temp2;
+//                 temp2=temp2.next;
+//             }
+//         }
+//         if(temp1 ==null){
+//             temp.next = temp2;
+//         }else{
+//             temp.next = temp1;
+//         }
+//         return dummy.next;
+
+//     }
+// }
+
+
+
+//////////////////////PRACTICE
+
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode temp1 = list1;
         ListNode temp2 = list2;
         ListNode dummy = new ListNode(0);
         ListNode temp = dummy;
-
-        while(temp1 !=null && temp2 != null){
+        while(temp1 != null && temp2 !=null){
             if(temp1.val <= temp2.val){
-                temp.next = temp1;
-                temp = temp1;      //ye hi difference hai that temp ko hi temp1 pe point
-                temp1=temp1.next;
+                temp.next =temp1;
+                temp=temp1;
+                temp1= temp1.next;
             }
             else{
                 temp.next = temp2;
                 temp = temp2;
-                temp2=temp2.next;
+                temp2 = temp2.next;
             }
         }
-        if(temp1 ==null){
-            temp.next = temp2;
-        }else{
+        if(temp1 == null){
+            temp.next =temp2;
+        }
+        else{
             temp.next = temp1;
         }
         return dummy.next;
