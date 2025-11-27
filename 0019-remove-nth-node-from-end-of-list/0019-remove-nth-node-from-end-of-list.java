f2dce385c65a235c6e565063794c9d60b1f7ecf9
SARVAN SINGH <thakurshravan164@gmail.com>
2025-11-27 20:58:02 +0530

    Time: 0 ms (100%), Space: 43.4 MB (27.39%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,25 +1,16 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head.next ==null && n==1){return head.next;}
         ListNode slow = head;
         ListNode fast = head;
-        for(int i=1;i<=n;i++){   //see that we travel till n+1 because so that slow nth element se ek element piche ruke ji
+        for(int i=1;i<=n;i++){   //see that we travel till n because so that slow nth element se ek element piche ruke ji
             fast = fast.next;
         }
-        if(fast == null){
+        if(fast == null){        //please check this condition as this will happen so means that n== length of node or something like that
             return head.next;
         }
-        while(fast.next != null){
+        while(fast.next != null){   //see that this is fast.next(not fast) because hame slow ko ek slow.prev pe rokna hai
             slow=slow.next;
             fast=fast.next;
         }
