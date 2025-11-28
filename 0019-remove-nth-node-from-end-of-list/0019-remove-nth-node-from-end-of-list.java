7ed34123bedbdecccb33d9f3010097a2e55cbfc5
SARVAN SINGH <thakurshravan164@gmail.com>
2025-11-28 14:45:38 +0530

    Time: 0 ms (100%), Space: 43.3 MB (28.48%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,20 +1,29 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head.next ==null && n==1){return head.next;}
-        ListNode slow = head;
+        ListNode slow =head;
         ListNode fast = head;
-        for(int i=1;i<=n;i++){   //see that we travel till n because so that slow nth element se ek element piche ruke ji
+        if(head==null){return head;}
+        if(head.next ==null && n==1){return null;}
+
+        for(int i=1;i<=n;i++){             //moving in forward direction by n positions
             fast = fast.next;
         }
-        if(fast == null){        //please check this condition as this will happen so means that n== length of node or something like that
-            return head.next;
+        if(fast ==null){return head.next;}
+        while(fast.next !=null){         //now one by one move slow and fast in forward direction  {condition should be fast.next} check on dry run
+            slow = slow.next;
+            fast = fast.next;
         }
-        while(fast.next != null){   //see that this is fast.next(not fast) because hame slow ko ek slow.prev pe rokna hai
-            slow=slow.next;
-            fast=fast.next;
-        }
-        slow.next = slow.next.next;
+        slow.next=slow.next.next;       
         return head;
     }
 }
