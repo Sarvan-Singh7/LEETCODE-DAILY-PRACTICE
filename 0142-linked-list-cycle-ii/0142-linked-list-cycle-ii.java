d84df7c9eec616ccb94b1f9855530ea26b5a2b33
SARVAN SINGH <thakurshravan164@gmail.com>
2025-11-29 16:28:25 +0530

    Time: 0 ms (100%), Space: 46.6 MB (16.7%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,21 +1,45 @@
 
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         if(slow == null  || slow.next == null ){return null;}  //condition check if not any node or only one node
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(slow == fast){    //have to break when both equal or share same adress
+//                 break;
+//             }
+//         }
+//         if(fast == null || fast.next == null){return null;}  //valid if somehow loop breaks or we can say loop not exists
+//         ListNode check = head;
+//         while(check != slow){     //new pointer which will move alongside slow and when both equal so node detected
+//             check  = check.next;
+//             slow = slow.next;
+//         }
+//         return slow;   //at last return slow;
+//     }
+// }
+
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
+        ListNode slow =head;
         ListNode fast = head;
-        if(slow == null  || slow.next == null ){return null;}
-        while(fast != null && fast.next != null){
-            slow = slow.next;
+        while(fast !=null && fast.next !=null){
+            slow=slow.next;
             fast = fast.next.next;
-            if(slow == fast){
+            if(slow==fast){
                 break;
             }
         }
-        if(fast == null || fast.next == null){return null;}
+        if(fast ==null || fast.next ==null){
+            return null;
+        }
         ListNode check = head;
-        while(check != slow){
-            check  = check.next;
-            slow = slow.next;
+        while(slow != check){
+            slow= slow.next;
+            check = check.next;
         }
         return slow;
     }
