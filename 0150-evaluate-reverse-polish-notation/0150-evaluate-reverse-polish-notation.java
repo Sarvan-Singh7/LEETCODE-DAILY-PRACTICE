b11e7a7d14a83ecfcfbd1a799b1e416728d8142f
SARVAN SINGH <thakurshravan164@gmail.com>
2025-12-02 23:58:40 +0530

    Time: 7 ms (35.35%), Space: 45.3 MB (26.01%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,32 +1,64 @@
+// class Solution {
+//     public int evalRPN(String[] tokens) {
+//         Stack<Integer> st = new Stack<>();
+//         int a,b;
+//         for(int i=0;i<tokens.length;i++){
+//             String t = tokens[i];          //store in String for easy reference
+//             if(t.equals("+")){            //as s[i] will not work in case of strings
+//                 b=st.pop();
+//                 a= st.pop();
+//                 st.push(a+b);
+//             }
+//             else if(t.equals("-")){
+//                 b=st.pop();
+//                 a=st.pop();
+//                 st.push(a-b);
+//             }
+//             else if(t.equals("*")){
+//                 b=st.pop();
+//                 a=st.pop();
+//                 st.push(a*b);
+//             }
+//             else if(t.equals("/")){
+//                 b=st.pop();
+//                 a=st.pop();
+//                 st.push(a/b);
+//             }
+//             else{    //har baar integer mein convert karke stack mein push karo
+//                int answer = Integer.parseInt(t);
+//                st.push(answer);
+//             }
+//         }
+//         return st.pop();
+//     }
+// }
+
+
+/////Another Method
+
+
 class Solution {
+    public boolean isNumber(String s){
+        if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") ){
+            return false;
+        }
+        return true;
+    }
     public int evalRPN(String[] tokens) {
         Stack<Integer> st = new Stack<>();
-        int a,b;
-        for(int i=0;i<tokens.length;i++){
-            String t = tokens[i];
-            if(t.equals("+")){
-                b=st.pop();
-                a= st.pop();
-                st.push(a+b);
+        for(String s : tokens){
+            if(isNumber(s)){
+                st.push(Integer.parseInt(s));
             }
-            else if(t.equals("-")){
-                b=st.pop();
-                a=st.pop();
-                st.push(a-b);
-            }
-            else if(t.equals("*")){
-                b=st.pop();
-                a=st.pop();
-                st.push(a*b);
-            }
-            else if(t.equals("/")){
-                b=st.pop();
-                a=st.pop();
-                st.push(a/b);
-            }
-            else{    //har baar integer mein convert karke stack mein push karo
-               int answer = Integer.parseInt(t);
-               st.push(answer);
+            else{
+                int x = st.pop();
+                int y = st.pop();
+                if(s.equals("+") ){
+                    st.push(y + x);
+                }
+                if(s.equals("-") ){st.push(y-x);}
+                if(s.equals("*") ){st.push(y*x);}
+                if(s.equals("/") ){st.push(y/x);}
             }
         }
         return st.pop();
