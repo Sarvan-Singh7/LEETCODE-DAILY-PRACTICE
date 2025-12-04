f7b524629517a667338a7f625c0d8da1da479a51
SARVAN SINGH <thakurshravan164@gmail.com>
2025-12-04 11:09:54 +0530

    Time: 2 ms (88.26%), Space: 49.3 MB (15.93%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,47 +1,43 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode reverseList(ListNode head){   //Reverse Logic
-        ListNode curr = head;
-        ListNode ahead=head;
-        ListNode prev = null;
-        while(curr != null){
-            ahead=curr.next;
+    public ListNode reverseList(ListNode head){
+        ListNode curr = head,ahead = head,prev = null;
+        while(curr !=null){
+            ahead = curr.next;
             curr.next = prev;
-            prev=curr;
-            curr=ahead;
+            prev = curr;
+            curr = ahead;
         }
         return prev;
-        
     }
     public void reorderList(ListNode head) {
-        ListNode slow = head;
+        ListNode slow  = head;
         ListNode fast = head;
-        while(fast != null && fast.next !=null){  //FInding Middle
-            slow=slow.next;
+        while(fast !=null && fast.next != null){    //finding middle to reverse half portion
+            slow = slow.next;
             fast = fast.next.next;
         }
-        // reversing the second half of the list
-       ListNode newNode = reverseList(slow.next);
-      // breaking the list from the middle
-        slow.next=null;
-   //merging both list
-       //first half list pointer
-       ListNode curr = head;
-       //second half list pointer
-       ListNode dummy = newNode;
-       while(curr!=null && dummy!=null){
-             //pointer to store next element of curr(1st half list)
-           ListNode temp = curr.next;
-           //link element of 1st half to that of second half
-           curr.next=dummy;
-           //pointer to store next element of dummy(2nd half list)
-           ListNode temp2=dummy.next;
-           //link the rest of the first half list
-           dummy.next=temp;
-           //increment curr and dummy pointer to do the same thing again and again util we reach end of the any one list or both list
-           curr=temp;
-           dummy=temp2;
-       }
-        
+        ListNode slowNext = reverseList(slow.next);   //reverse the nodes function and n reverse always pass slow (not slow.next)
+        slow.next = null;                            //Very Important to do so that list now divided into two lists
+        ListNode curr = head;
+        ListNode rev = slowNext; 
+
+        while(curr !=null && rev != null){        //when any of list empty so stop loop
+            ListNode temp = curr.next;
+            ListNode dummy2 = rev.next;           //storing current of both to do so 
+            curr.next = rev;
+            rev.next = temp;
+            curr = temp;
+            rev = dummy2;
+        } 
     }
 }
