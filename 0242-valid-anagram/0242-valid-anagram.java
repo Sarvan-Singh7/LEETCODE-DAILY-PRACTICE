a388b91ade13f93d24716a0ff25eecd9ef2b5bac
SARVAN SINGH <thakurshravan164@gmail.com>
2025-12-08 12:52:03 +0530

    Time: 15 ms (25.25%), Space: 46.4 MB (18.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,39 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()){return false;}
+//         int Freq[] = new int[26];
+//         for(int i=0;i<s.length();i++){
+//             Freq[s.charAt(i) - 'a']++;
+//             Freq[t.charAt(i) -'a']--;
+//         }
+//         for(int i=0;i<26;i++){
+//             if(Freq[i] != 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){return false;}
-        int Freq[] = new int[26];
+        HashMap<Character , Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            Freq[s.charAt(i) - 'a']++;
-            Freq[t.charAt(i) -'a']--;
+            char ch = s.charAt(i);
+            map.put(ch , map.getOrDefault(ch , 0) + 1);
         }
-        for(int i=0;i<26;i++){
-            if(Freq[i] != 0){
+        for(int i=0;i<s.length();i++){
+            char ch = t.charAt(i);
+            map.put(ch , map.getOrDefault(ch , 0) - 1);
+        }
+        
+        for(char key : map.keySet()){
+            if(map.get(key) !=0){
                 return false;
             }
         }
         return true;
-
     }
 }
