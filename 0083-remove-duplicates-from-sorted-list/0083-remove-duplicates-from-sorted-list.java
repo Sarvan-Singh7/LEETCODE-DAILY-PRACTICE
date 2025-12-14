598ae4c025c86bc5901b3be1583183f4f160254d
SARVAN SINGH <thakurshravan164@gmail.com>
2025-12-14 21:47:09 +0530

    Time: 0 ms (100%), Space: 45.3 MB (57.4%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -1,7 +1,30 @@
 
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if(head == null ){
+//             return head;
+//         }
+//         ListNode i = head;
+//         ListNode j = head.next;
+//         while(j != null){
+//             if(i.val == j.val){
+//                 j=j.next;
+//             }
+//             else if(i.val !=j.val){
+//                 i.next = j;
+//                 i=j;
+//                 j=j.next;
+//             }
+//         }
+//         i.next = null;     //mandate step to disconnect nodes after operation.
+//         return head;
+//     }
+// }
+
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null ){
+        if(head == null || head.next == null){
             return head;
         }
         ListNode i = head;
@@ -10,7 +33,7 @@ class Solution {
             if(i.val == j.val){
                 j=j.next;
             }
-            else if(i.val !=j.val){
+            else{
                 i.next = j;
                 i=j;
                 j=j.next;
