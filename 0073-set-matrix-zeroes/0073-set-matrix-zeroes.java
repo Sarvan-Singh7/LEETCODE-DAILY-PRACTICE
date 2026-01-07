de46313d9fc115868db1744de42e873e22b50606
SARVAN SINGH <thakurshravan164@gmail.com>
2026-01-07 14:36:04 +0530

    Time: 0 ms (100%), Space: 47.5 MB (15.62%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -45,24 +45,51 @@
 
 
 
+// class Solution {
+
+//     public void setZeroes(int[][] matrix) {
+//         int n= matrix.length;
+//         int m= matrix[0].length;
+//         int []row = new int[n];
+//         int []column = new int[m];
+//         for(int i=0;i<n;i++){                   //pehle loop chala kar row and column matrix mein woh rows and column mark kar liye jo ki zero hai as 1
+//             for(int j=0;j<m;j++){
+//                 if(matrix[i][j] == 0){
+//                     row[i] = 1;
+//                     column[j] =1;
+//                 }
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(row[i] ==1 || column[j] ==1){   //now jo bhi 1 hai un sab ki rows and columns ke element ko zero kar do
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+        
+       
+//     }
+// }
+
 class Solution {
 
     public void setZeroes(int[][] matrix) {
         int n= matrix.length;
-        int m= matrix[0].length;
-        int []row = new int[n];
-        int []column = new int[m];
-        for(int i=0;i<n;i++){                   //pehle loop chala kar row and column matrix mein woh rows and column mark kar liye jo ki zero hai as 1
+        int m = matrix[0].length;
+        int row[] = new int[n];
+        int column[] = new int[m];
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j] == 0){
                     row[i] = 1;
-                    column[j] =1;
+                    column[j] = 1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(row[i] ==1 || column[j] ==1){   //now jo bhi 1 hai un sab ki rows and columns ke element ko zero kar do
+                if(row[i] ==1 || column[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
