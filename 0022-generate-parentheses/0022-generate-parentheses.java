14df76576e31eb84e9c578833069031faa3dadfc
SARVAN SINGH <thakurshravan164@gmail.com>
2026-01-17 15:48:09 +0530

    Time: 2 ms (67.53%), Space: 45 MB (18.3%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,6 +1,7 @@
 class Solution {
+    //main logic is that whenever opening is leass than n and closing is less than opening count so i can say that whenever both equal then it will be added to arraylist
     public void generate(ArrayList<String> arr , int n, int oc, int cc, String s){
-        if(oc == n && cc == n){
+        if(oc == n && cc == n){   // oc = opening count, cc = closing count
             arr.add(s);
             return;
         }
@@ -15,6 +16,7 @@ class Solution {
         ArrayList<String> arr = new ArrayList<>();
         int oc=0,cc=0;
         generate(arr,n,oc,cc,"");
+        System.out.println(arr.size());  // so to return count;
         return arr;
     }
 }
