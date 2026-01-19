c2b38e0542f6762336384ed32c258392a5b13709
SARVAN SINGH <thakurshravan164@gmail.com>
2026-01-19 23:31:24 +0530

    Time: 20 ms (38.81%), Space: 47.7 MB (49.78%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,18 +1,18 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int answer = -1;
+        int answer = -1;      // used to get key of maximum frequency element
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(nums[i] %2 ==0)
+            if(nums[i] %2 ==0)                                 // so that i stored only for even elements
             map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
-        int maxFreq =0;
+        int maxFreq =0;     // always used to get maximum frequency
         for(int key : map.keySet()){
             if(map.get(key)> maxFreq){
                 maxFreq = map.get(key);
                 answer = key;
             }
-            if(maxFreq == map.get(key) && answer > key){
+            if(maxFreq == map.get(key) && answer > key){   // used to handle if smaller element then current is having same frequency
                 answer = key;
             }
         }
