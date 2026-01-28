cb6d3ade191f3e535a122bdf1254bb4e48fad4ca
SARVAN SINGH <thakurshravan164@gmail.com>
2026-01-28 14:41:51 +0530

    Time: 1 ms (88.1%), Space: 44.4 MB (15.13%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,19 +2,19 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
         List<Integer> helper = new ArrayList<>();
-         Calculate(answer, nums,helper, 0);
+        Calculate(nums, answer,helper, 0);
         return answer;
     }
-    public static void Calculate(List<List<Integer>>answer, int nums[],List<Integer> helper, int si){
-        if(si == nums.length){
+    public static void Calculate(int nums[], List<List<Integer>> answer,List<Integer> helper, int i){
+        if(i == nums.length){
             answer.add(new ArrayList<>(helper));
-            return;
+            return;/// return mean ab bactrack karke as upar bhi jaega
         }
-        int Adder = nums[si];
-        
-        Calculate(answer,nums, helper, si+1);
-        helper.add(Adder);      // added in between agar upar kiya so not true
-        Calculate(answer, nums, helper, si+1);
-        helper.remove(helper.size() -1);   // removing it is mandate so that not append helper array
+        //// Include case first
+        helper.add(nums[i]);
+        Calculate(nums, answer, helper, i+1); // helper.add() inside kiya agar so erroer
+        helper.remove(helper.size() -1);      // excluse last elemnt before start exclude for fair
+        Calculate(nums, answer, helper, i+1);
+
     }
 }
