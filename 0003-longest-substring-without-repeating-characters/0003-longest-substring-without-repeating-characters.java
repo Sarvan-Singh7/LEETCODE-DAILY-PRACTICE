20825d99e3120e55fc48182c74acca26a4dfb747
SARVAN SINGH <thakurshravan164@gmail.com>
2026-02-15 22:54:02 +0530

    Time: 2 ms (97.7%), Space: 45.3 MB (83.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,19 +1,39 @@
+// class Solution {    ====== BRUTE SOLUTION
+//     public int lengthOfLongestSubstring(String s) {
+//         if(s==null || s.length() ==0)return 0;
+//         int maxLength = Integer.MIN_VALUE;
+//         for(int i=0;i<s.length();i++){
+//             int Hashing[] = new int[256];
+            
+//             for(int j=i;j<s.length();j++){
+//                 int index = s.charAt(j);
+//                 if(Hashing[index] ==1){
+//                     break;
+//                 }
+//                 maxLength = Math.max(maxLength, j-i+1);
+//                 Hashing[index] =1;
+//             }
+//         }
+//         return maxLength;
+//     }
+// }
+
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s==null || s.length() ==0)return 0;
-        int maxLength = Integer.MIN_VALUE;
-        for(int i=0;i<s.length();i++){
-            int Hashing[] = new int[256];
-            
-            for(int j=i;j<s.length();j++){
-                int index = s.charAt(j);
-                if(Hashing[index] ==1){
-                    break;
-                }
-                maxLength = Math.max(maxLength, j-i+1);
-                Hashing[index] =1;
+        int l=0,r=0;
+        int maxlen = 0;
+        int hash[] = new int[256];
+        Arrays.fill(hash, -1);///all to -1 so that can update array at new value
+        while(r<s.length()){
+            if(hash[s.charAt(r)] >= l){
+                l = Math.max(l, hash[s.charAt(r)] +1);
             }
+
+            maxlen = Math.max(maxlen, r-l+1);
+            hash[s.charAt(r)]  = r;
+            r++;
         }
-        return maxLength;
+        return maxlen;
     }
 }
