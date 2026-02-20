1f4adb36940370606b4ce322bdfa168edd4c5677
SARVAN SINGH <thakurshravan164@gmail.com>
2026-02-20 21:41:49 +0530

    Time: 55 ms (43.71%), Space: 70.7 MB (29.79%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,51 @@
+// class Solution {
+//     public int totalFruit(int[] fruits) {
+        
+//         int maxLength =0;
+//         for(int i=0;i<fruits.length;i++){
+//             HashSet<Integer> map = new HashSet<>();
+//             int count=0;
+//             for(int j=i;j<fruits.length;j++){
+//                 map.add(fruits[j]);
+//                 if(map.size() <=2){
+//                     maxLength = Math.max(maxLength, j-i+1);
+//                 }
+//                 else{
+//                     break;   // BREAK JARUR LAGAO AS U CAN STOP WHEN NOT NEEDED
+//                 }
+                
+//             }
+//         }
+//         return maxLength;
+        
+        
+//     }
+// }
+
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int maxLength =0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int left =0;
+        for(int right=0;right<fruits.length;right++){
+            map.put(fruits[right], map.getOrDefault(fruits[right],0) +1);
+            if(map.size() <=2){
+                maxLength= Math.max(maxLength, right - left +1);
+            }else{
+                while(map.size()>2){
+                    
+                    map.put(fruits[left], map.get(fruits[left]) -1);
+                    if(map.get(fruits[left]) ==0){
+                        map.remove(fruits[left]);
+                    }
+                    left++;
+                }
+            }
+        }
+        return maxLength;
+    }
+}
+
+
+
