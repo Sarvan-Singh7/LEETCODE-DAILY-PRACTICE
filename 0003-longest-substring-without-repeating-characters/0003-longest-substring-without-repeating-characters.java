678a422768e2f66187eac5aa1670b3bb20ad2221
SARVAN SINGH <thakurshravan164@gmail.com>
2026-02-20 11:12:58 +0530

    Time: 6 ms (68.95%), Space: 46.4 MB (47.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -19,21 +19,43 @@
 // }
 
 
+
+
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int l=0,r=0;
-        int maxlen = 0;
-        int hash[] = new int[256];
-        Arrays.fill(hash, -1);///all to -1 so that can update array at new value
-        while(r<s.length()){
-            if(hash[s.charAt(r)] >= l){
-                l = Math.max(l, hash[s.charAt(r)] +1);
-            }
-            
-            maxlen = Math.max(maxlen, r-l+1);
-            hash[s.charAt(r)]  = r;
-            r++;
-        }
-        return maxlen;
+    public int lengthOfLongestSubstring(String str) {
+        HashSet<Character>hm=new HashSet<>();
+        int start=0;
+        int max=0;
+        
+        
+     for(int i=0;i<str.length();i++){
+         char ch=str.charAt(i);
+         while(hm.contains(ch)){
+             hm.remove(str.charAt(start));
+             start++;
+         } 
+         hm.add(ch);
+         max=Math.max(max,i-start+1);
+     }
+     return max;
     }
 }
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int l=0,r=0;
+//         int maxlen = 0;
+//         int hash[] = new int[256];
+//         Arrays.fill(hash, -1);///all to -1 so that can update array at new value
+//         while(r<s.length()){
+//             if(hash[s.charAt(r)] >= l){      //so that to avoid duplicates
+//                 l = Math.max(l, hash[s.charAt(r)] +1);  // math.max only works here
+//             }
+            
+//             maxlen = Math.max(maxlen, r-l+1);
+//             hash[s.charAt(r)]  = r;
+//             r++;
+//         }
+//         return maxlen;
+//     }
+// }
