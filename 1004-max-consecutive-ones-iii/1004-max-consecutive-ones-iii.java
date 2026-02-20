c26bed70950d3b58d9db280040a9181e2a3ab0c4
SARVAN SINGH <thakurshravan164@gmail.com>
2026-02-20 12:04:44 +0530

    Time: 3 ms (98.08%), Space: 52.5 MB (16.64%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,20 +1,42 @@
-class Solution {
+class Solution {  
     public int longestOnes(int[] nums, int k) {
-        int maxlen =0;
+        int maxLength =0;
+        int zeroes=0;
+        int start=0;
         for(int i=0;i<nums.length;i++){
-            int count=0;
-            
-            for(int j=i;j<nums.length;j++){
-                if(nums[j] == 0 ){ // 0 calculate karte raho
-                    count++;
-                }
-                if(count<=k){   //jab tak 0 k se chote so update maxlength
-                    maxlen = Math.max(maxlen, j-i+1);
-                }
-                else{break;}
+            if(nums[i] == 0){
+                zeroes++;
             }
+            while(zeroes>k){               
+               if(nums[start++] ==0){
+                zeroes--;
+               }
+            }
+            maxLength = Math.max(maxLength, i-start+1);
         }
-        return maxlen;
+        return maxLength;
     }
 
 }
+
+
+// class Solution {  //== Brute code But stilll Run Because for Break condition reduce TC
+//     public int longestOnes(int[] nums, int k) {
+//         int maxlen =0;
+//         for(int i=0;i<nums.length;i++){
+//             int count=0;
+            
+//             for(int j=i;j<nums.length;j++){
+//                 if(nums[j] == 0 ){ // 0 calculate karte raho
+//                     count++;
+//                 }
+//                 if(count<=k){   //jab tak 0 k se chote so update maxlength
+//                     maxlen = Math.max(maxlen, j-i+1);
+//                 }
+//                 else{break;}  //for optimization it is necccesary because without it some cases not pass longer
+//             }
+//         }
+//         return maxlen;
+//     }
+
+// }
