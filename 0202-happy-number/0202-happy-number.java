18e1f390b3cd2deedfbf8196313b160941c578f3
SARVAN SINGH <thakurshravan164@gmail.com>
2026-02-23 20:56:35 +0530

    Time: 2364 ms (2.69%), Space: 42.6 MB (49.8%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isHappy(int n) {
         
-        int count = 8;
+        int count = 800000; /// as given integer ka size 8 se bada nahin ho sakta
         while(count-- > 0){int sum =0;
         while(n>0){
             int item = n % 10;
