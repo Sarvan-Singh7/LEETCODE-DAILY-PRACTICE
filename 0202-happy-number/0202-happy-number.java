9a8be3bfd9c772f1a7a47b7e7ad6c0921d6e6f86
SARVAN SINGH <thakurshravan164@gmail.com>
2026-02-23 20:36:59 +0530

    Time: 31 ms (2.69%), Space: 42.7 MB (32.81%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        int count = 10000;
+        while(count-- > 0){int sum =0;
+        while(n>0){
+            int item = n % 10;
+            sum += (item * item);
+            n/=10;
+        }
+        if(sum ==1)return true;
+        n = sum;
+    }
+    return false;
+    }
+}
