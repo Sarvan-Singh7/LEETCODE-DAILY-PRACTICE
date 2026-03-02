ef44cab0b889773e5fd03ad70b8f2d8ce02ba690
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-02 13:38:24 +0530

    Time: 3 ms (61%), Space: 52.8 MB (24.91%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,39 +1,41 @@
-// class Solution {
-//     public int findMaxConsecutiveOnes(int[] nums) {
-//         int n=nums.length;
-//         int maxi=Integer.MIN_VALUE;
-//         int count=0;
-//         for(int i=0;i<n;i++){
-//             count++;
-//             if(nums[i] ==0){
-//                 count=0;
-//             }
-            
-//             maxi=Math.max(maxi,count);
-            
-//         }
-//         return maxi;
-//     }
-// }
-
-
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int maxCount = 0;
-        int InnerCount=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] ==0){
-                InnerCount=0;
-                continue;
+        int n=nums.length;
+        int maxi=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 1){
+                count++;
             }
             else{
-                InnerCount++;
+                
+                count=0;
+                continue;
             }
-            maxCount = Math.max(InnerCount, maxCount);
+            maxi = Math.max(maxi, count);   // ye else part mein calculate kiya so error aya so do AT THIS
         }
-        return maxCount;
+        return maxi;
     }
 }
+
+
+// class Solution {
+//     public int findMaxConsecutiveOnes(int[] nums) {
+//         int maxCount = 0;
+//         int InnerCount=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i] ==0){
+//                 InnerCount=0;
+//                 continue;
+//             }
+//             else{    //do not forgot to calculate ones count also
+//                 InnerCount++;
+//             }
+//             maxCount = Math.max(InnerCount, maxCount);
+//         }
+//         return maxCount;
+//     }
+// }
 
 
 
