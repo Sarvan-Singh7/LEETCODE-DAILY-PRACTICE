dfd1b03f67323c15ad77a5ee6c2eec9cfe142258
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-06 10:40:09 +0530

    Time: 1 ms (99.84%), Space: 55.9 MB (17.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,3 +1,21 @@
+class Solution {      ////MOST OPTIMAL MOORE VOTING ALGORITHM
+    public int majorityElement(int[] nums) {
+        int count =0;
+        int answer = nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(count ==0){
+                count =1;
+                answer = nums[i];
+            } //for starting as well as when all cancell out
+            else if(nums[i] == answer)count++;  //increase count when find same to reach upper condition with help of cancellation with below one
+            else count--;    //help in making count =0 for selection of new element
+        }
+        return answer;
+    }
+}
+
+
+
 // class Solution {      ////BRUTE HAI SIR
 //     public int majorityElement(int[] nums) {
 //         int maxCount=0;
@@ -18,18 +36,16 @@
 // }
 
 
-class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        }
-        for(int key : map.keySet()){
-            if(map.get(key) > nums.length/2){
-                return key;
-            }
-        }
-        return -1;
-}
-}
+// class Solution {    ///SOLUTION USING HASHMAP
+//     public int majorityElement(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             map.put(nums[i], map.getOrDefault(nums[i], 0)+1);   //pehle add kiya and baad mein check kiya hai
+//             if(map.get(nums[i]) > nums.length/2){
+//                 return nums[i];
+//             }
+//         }
+        
+//         return -1;
+// }
+// }
