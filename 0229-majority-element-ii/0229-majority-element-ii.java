9a4b3c8d3ea0a1a7bceab7ad8b811f789b2935b5
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-18 14:36:49 +0530

    Time: 19 ms (11.91%), Space: 53.1 MB (18.74%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,20 +1,44 @@
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         int n = nums.length;
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+//              if(map.get(nums[i]) >n/3){
+//                 if(!arr.contains(nums[i])){   ///check kar lo ki woh pehle se arraylist mein present na ho 
+//                     arr.add(nums[i]);
+//                 }
+                
+//             }
+//         }
+//         for(int key : map.keySet()){
+           
+//         }
+//         return arr;
+//     }
+// }
+
+
 class Solution {
+    // Function to find majority elements in an array
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> arr = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-             if(map.get(nums[i]) >n/3){
-                if(!arr.contains(nums[i])){
-                    arr.add(nums[i]);
-                }
-                
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> mpp = new HashMap<>();
+        int mini = n / 3 + 1;
+
+        for (int i = 0; i < n; i++) {
+            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+
+            // Add to result only when the count just reaches mini
+            if (mpp.get(nums[i]) == mini) {
+                result.add(nums[i]);
             }
+
+            if (result.size() == 2) break;
         }
-        for(int key : map.keySet()){
-           
-        }
-        return arr;
+
+        return result;
     }
 }
