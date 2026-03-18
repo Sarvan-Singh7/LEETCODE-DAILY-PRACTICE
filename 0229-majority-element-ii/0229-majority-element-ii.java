6ce1977f4408a4fcf3341b305b64848833525ee0
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-18 14:33:41 +0530

    Time: 23 ms (6.45%), Space: 52.3 MB (79.77%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -5,11 +5,15 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+             if(map.get(nums[i]) >n/3){
+                if(!arr.contains(nums[i])){
+                    arr.add(nums[i]);
+                }
+                
+            }
         }
         for(int key : map.keySet()){
-            if(map.get(key) >n/3){
-                arr.add(key);
-            }
+           
         }
         return arr;
     }
