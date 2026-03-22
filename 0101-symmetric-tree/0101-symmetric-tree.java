f9069da02efcfb7e82cbc6f1873017cb41679c46
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-22 11:30:14 +0530

    Time: 0 ms (100%), Space: 43.6 MB (46.24%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,29 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null)return true;
-        return HelpSymmetric(root.left, root.right);
+        return HelpSymmetric(root.left, root.right);  //leftSubTree and rightSubTree ka partition lagakar bhej diya 2 trees from 1
     }
-    public boolean HelpSymmetric(TreeNode leftNode, TreeNode rightNode){
-        if(leftNode == null || rightNode == null){
-            return leftNode  == rightNode;
+    public boolean HelpSymmetric(TreeNode leftNode, TreeNode rightNode){ //checking that 2 trees are identical or not
+        if(leftNode == null || rightNode == null){//agar ek condition true matlab ek null pe and ek nahin so return false as == used below
+            return leftNode  == rightNode; //agar dono null pe toh hi true
         }
-        if(leftNode.val != rightNode.val)return false;
+        if(leftNode.val != rightNode.val)return false;  //value not match so return false
 
         return HelpSymmetric(leftNode.left, rightNode.right) && HelpSymmetric(leftNode.right, rightNode.left);
+        //upper traversal mein ek node ka left badhao and dusre ka right as mirror hai center se .
     }
 }
