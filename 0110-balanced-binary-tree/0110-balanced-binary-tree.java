49b88ed8ee238a7adbc48241948536b08f76f9e7
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-23 11:42:12 +0530

    Time: 0 ms (100%), Space: 45.6 MB (58.22%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,29 +1,42 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-        if(root == null)return true;
-        int leftSubTreeDepth = levels(root.left);
-        int rightSubTreeDepth = levels(root.right);
-        if(Math.abs(leftSubTreeDepth - rightSubTreeDepth) >=2)return false;
+//SSE CAREFULLY AS BALANCED MEAN THAT LEFT AND RIGHT SUBTREE ANY LEVEL MUST HAVE HEIGHT OR DEPTH <= 1 
 
-        return isBalanced(root.left) && isBalanced(root.right);
+/////BELOW IS OPTIMAL CODE WITH HELP OF LEVES CALCULATE CODE
+class Solution {
+    static boolean flag = false;
+    public boolean isBalanced(TreeNode root) {   ////COMPLEXITY = O(N SQUARE) BECAUSE ONE CALL GOES FOR O(N) SO INNER CALL FOR LEVELS CALCULATION ALSO
+        if(root == null)return true;
+        flag = true;
+        levels(root);
+        if(flag == false)return false;
+        return true;
     }
     public int levels(TreeNode root){
         if(root == null)return 0;
-        return 1 + Math.max(levels(root.left), levels(root.right));
+        int left = levels(root.left);
+        int right = levels(root.right);
+        if(Math.abs(left - right) >=2)flag = false;
+        return 1 + Math.max(left, right);
     }
+  
 }
+
+
+
+
+
+//NOW SO IN FUNCTION I CALCULATED HEIGHT FOR EACH SUBTREE AND THEN CAMPARE ITS ABSOLUTE VALUE O CHECK IS IT VALID
+//AND RETURN APPROPRIATELY
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {   ////COMPLEXITY = O(N SQUARE) BECAUSE ONE CALL GOES FOR O(N) SO INNER CALL FOR LEVELS CALCULATION ALSO
+//         if(root == null)return true;
+//         int leftSubTreeDepth = levels(root.left);
+//         int rightSubTreeDepth = levels(root.right);
+//         if(Math.abs(leftSubTreeDepth - rightSubTreeDepth) >=2)return false;
+
+//         return isBalanced(root.left) && isBalanced(root.right);
+//     }
+//     public int levels(TreeNode root){
+//         if(root == null)return 0;
+//         return 1 + Math.max(levels(root.left), levels(root.right));
+//     }
+// }
