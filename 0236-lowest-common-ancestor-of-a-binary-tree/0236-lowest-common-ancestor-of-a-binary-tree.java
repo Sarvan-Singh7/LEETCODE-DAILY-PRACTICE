44088d286d0c7c1ecf6df6cd599e0dbc17b5f235
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-28 17:39:28 +0530

    Time: 408 ms (5.03%), Space: 46.4 MB (89.79%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,14 +1,14 @@
 
-class Solution {
+class Solution {   //nearabout o(n)
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null)return null;
-        if(root.val == p.val || root.val == q.val)return root;
+        if(root.val == p.val || root.val == q.val)return root;   //if at any point we are at point when root becomes p q o finded
         boolean pLiesInLeft = check(root.left,p);
-        boolean qLiesInRight = check(root.left,q);
-        if(pLiesInLeft && qLiesInRight){
+        boolean qLiesInRight = check(root.right,q);  //see we are not checking in rightsubtree 
+        if(pLiesInLeft && !qLiesInRight){           //if both in left then defanately find in next iteration leftwards
             return lowestCommonAncestor(root.left, p, q);
         }
-        else if(!pLiesInLeft && !qLiesInRight){
+        else if(!pLiesInLeft && qLiesInRight){   /////if in rightsub tree
             return lowestCommonAncestor(root.right, p, q);
         }
         else{
