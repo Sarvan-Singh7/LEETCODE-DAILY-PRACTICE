7ebddb38f953e788c4d466a13c19dd46035a2e09
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-28 14:56:03 +0530

    Time: 0 ms (100%), Space: 47 MB (80.39%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,17 +1,39 @@
 
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null)return 0;
-        int a1 =helperLevel(root.left) + helperLevel(root.right);
-        int a2 = diameterOfBinaryTree(root.left);
-        int a3 =diameterOfBinaryTree(root.right);
-        return Math.max(a1, Math.max(a2,a3));
+    static int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {  //== O(n2)  //as 2 times recursion calls
+        if(root == null)return 0;     ///it works as because calculating har level ka left subtree and right subtree
+        diameter =0;
+        helperLevel(root);
+        return diameter;
     }
     public int helperLevel(TreeNode p){
         if(p == null)return 0;
-        return 1 + Math.max(helperLevel(p.left), helperLevel(p.right));
+        int leftST =helperLevel(p.left);
+        int rightST = helperLevel(p.right);
+        diameter = Math.max(diameter, leftST + rightST);
+        return 1 + Math.max(leftST, rightST);
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int diameterOfBinaryTree(TreeNode root) {  //== O(n2)  //as 2 times recursion calls
+//         if(root == null)return 0;     ///it works as because calculating har level ka left subtree and right subtree
+//         int a1 =helperLevel(root.left) + helperLevel(root.right);   //below 4 lines do preorder traversal to visit each node to calculate diamter at each point
+//         int a2 = diameterOfBinaryTree(root.left);
+//         int a3 =diameterOfBinaryTree(root.right);
+//         return Math.max(a1, Math.max(a2,a3));  //at last max of all returned
+//     }
+//     public int helperLevel(TreeNode p){
+//         if(p == null)return 0;
+//         return 1 + Math.max(helperLevel(p.left), helperLevel(p.right));
+//     }
+// }
 
 
 
