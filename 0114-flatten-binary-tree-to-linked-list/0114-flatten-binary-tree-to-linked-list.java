03f4165b0b1dc4353aab51f60420000c148aebc7
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-29 17:32:59 +0530

    Time: 0 ms (100%), Space: 44 MB (44.69%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,37 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {   //==Simply Convert it to Skewed Binary tree so that it will be possibel inPlace
     public void flatten(TreeNode root) {
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        preOrder(root, arr);
-        for(int i=0;i<arr.size() - 1;i++){
-            TreeNode a = arr.get(i);
-            TreeNode b = arr.get(i+1);
-            a.right =b;
-            a.left = null;
-        }
-        // TreeNode last = arr.get(arr.size() -1);
-        // last.right = null;
-        // last.left = null;
+       if(root == null)return;
+       TreeNode lst = root.left;
+       TreeNode rst = root.right;
+       root.left = null;
+       flatten(lst);
+       flatten(rst);
+       root.right = lst;
+       TreeNode last = root;
+       while(last.right != null){
+        last = last.right;
+       }
+       last.right = rst;
 
     }
-    public static void preOrder(TreeNode root, ArrayList<TreeNode> arr){
-        if(root == null)return ;
-        arr.add(root);
-        preOrder(root.left, arr);
-        preOrder(root.right, arr);
-    }
+    
 }
+// class Solution {   //==Simply Convert it to Skewed Binary tree so that it will be possibel inPlace
+//     public void flatten(TreeNode root) {
+//         ArrayList<TreeNode> arr = new ArrayList<>();
+//         preOrder(root, arr);    //smae node reference passed so it is sure that pass by reference so that a refer to root in next step
+//         for(int i=0;i<arr.size() - 1;i++){
+//             TreeNode a = arr.get(i);
+//             TreeNode b = arr.get(i+1);
+//             a.right =b;
+//             a.left = null;
+//         }
+        
+
+//     }
+//     public static void preOrder(TreeNode root, ArrayList<TreeNode> arr){
+//         if(root == null)return ;
+//         arr.add(root);
+//         preOrder(root.left, arr);
+//         preOrder(root.right, arr);
+//     }
+// }
