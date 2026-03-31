c04c82573649a3a322671b420df50d87f6cf5ddc
SARVAN SINGH <thakurshravan164@gmail.com>
2026-03-31 10:53:34 +0530

    Time: 1 ms (6.96%), Space: 42.9 MB (89.27%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,28 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//ITERATIVE SOLUTION LIKE BFS APPROACH
 class Solution {
-    public static void helper(ArrayList<Integer> arr, TreeNode root){
-        if(root == null)return;
-        arr.add(root.val);
-        helper(arr, root.left);
-        helper(arr, root.right);
-    }
+    
     public List<Integer> preorderTraversal(TreeNode root) {
         ArrayList<Integer> arr = new ArrayList<>();
-        helper(arr, root);
+        if(root == null)return arr;
+        
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(st.size() > 0){
+            TreeNode current = st.pop();
+            arr.add(current.val);
+            if(current.right != null)st.push(current.right);
+            if(current.left != null)st.push(current.left);
+        }
         return arr;
     }
+    
 }
+////RECURSIVE SOLUTION AS IT IS
+// class Solution {
+//     public static void helper(ArrayList<Integer> arr, TreeNode root){
+//         if(root == null)return;
+//         arr.add(root.val);
+//         helper(arr, root.left);
+//         helper(arr, root.right);
+//     }
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         helper(arr, root);
+//         return arr;
+//     }
+// }
