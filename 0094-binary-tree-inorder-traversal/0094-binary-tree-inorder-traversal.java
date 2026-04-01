9bbbe14604ea7a34b8bdccd6ef21da917e86ddc8
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-01 11:36:35 +0530

    Time: 1 ms (6.44%), Space: 43.2 MB (41.59%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,28 +1,43 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public static void helper(List<Integer> arr, TreeNode root){
-        if(root == null)return;
-        helper(arr, root.left);
-        arr.add(root.val);
-        helper(arr, root.right);
-    }
+    //inorder when current.left == null
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        helper(arr, root);
-        return arr;
+        List<Integer> answer = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode current = root;
+        while(current != null  ||   st.size() >0){  ///do in this sequence and && cannot be used instead of ||
+            if(current != null){
+                if(current.left != null){
+                    st.push(current);
+                    current = current.left;
+                }else{
+                    answer.add(current.val);
+                    current = current.right;
+                }
+            }
+            else{
+                TreeNode top = st.pop();
+                answer.add(top.val);
+                current = top.right;
+            }
+        }
+        return answer;
     }
-}
+ }
+
+
+
+
+
+// class Solution {
+//     public static void helper(List<Integer> arr, TreeNode root){
+//         if(root == null)return;
+//         helper(arr, root.left);
+//         arr.add(root.val);
+//         helper(arr, root.right);
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> arr = new ArrayList<>();
+//         helper(arr, root);
+//         return arr;
+//     }
+//  }
