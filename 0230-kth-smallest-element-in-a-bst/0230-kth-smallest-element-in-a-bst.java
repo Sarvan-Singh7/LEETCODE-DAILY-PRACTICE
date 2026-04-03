0ed214389c6d4a35fc59886c966e4c1bf6e3fc30
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-03 11:44:07 +0530

    Time: 0 ms (100%), Space: 46.8 MB (41.55%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,15 +1,36 @@
 
 class Solution {
+    int k2;int answer;
     public int kthSmallest(TreeNode root, int k) {
+        k2 =k;
         ArrayList<Integer> arr = new ArrayList<>();
         if(root == null)return 0;
-        Inorder(root, arr);
-        return arr.get(k-1);
+        Inorder(root);
+        return answer;
     }
-    public void Inorder(TreeNode root, ArrayList<Integer> arr){
+    public void Inorder(TreeNode root){
         if(root == null)return;
-        Inorder(root.left,arr);
-        arr.add(root.val);
-        Inorder(root.right, arr);
+        Inorder(root.left);
+        k2--;   ///each time index decrease so it comes to that point
+        if(k2 ==0)answer = root.val;
+        Inorder(root.right);
     }
 }
+
+
+
+
+// class Solution {   === O(n) SC but using extra array
+//     public int kthSmallest(TreeNode root, int k) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         if(root == null)return 0;
+//         Inorder(root, arr);
+//         return arr.get(k-1);
+//     }
+//     public void Inorder(TreeNode root, ArrayList<Integer> arr){
+//         if(root == null)return;
+//         Inorder(root.left,arr);
+//         arr.add(root.val);
+//         Inorder(root.right, arr);
+//     }
+// }
