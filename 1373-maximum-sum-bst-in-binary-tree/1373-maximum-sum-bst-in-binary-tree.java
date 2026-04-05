73c3383c4870b656a2124c88ee3f175095f3f889
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-05 23:31:21 +0530

    Time: 7 ms (51.28%), Space: 63.8 MB (60.48%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -11,21 +11,21 @@ class Pair{
     }
 }
 class Solution {
-    static int maxSum;
+    static int maxSum; 
     public int maxSumBST(TreeNode root) {
-        maxSum=0;
+        maxSum=0;     //u have to initialize it here otherwise works different
         Validate(root);
         return maxSum;
     }
-    public Pair Validate(TreeNode root){
+    public Pair Validate(TreeNode root){   //postORDER Traversal
         if(root == null)return new Pair(Integer.MIN_VALUE, Integer.MAX_VALUE,0,true);
         Pair lst = Validate(root.left);
         Pair rst = Validate(root.right);
         int max = Math.max(root.val, Math.max(lst.max,rst.max));
         int min = Math.min(root.val, Math.min(lst.min,rst.min));
         int size = root.val + lst.sum + rst.sum;
-        boolean isTrue = lst.isBST && rst.isBST && (lst.max < root.val) && (rst.min > root.val);
-        if(isTrue){
+        boolean isTrue = lst.isBST && rst.isBST && (lst.max < root.val) && (rst.min > root.val); //to make sure it is valid
+        if(isTrue){       //if validated then add its sum
             maxSum = Math.max(maxSum, size);
         }
         return new Pair(max, min, size, isTrue);
