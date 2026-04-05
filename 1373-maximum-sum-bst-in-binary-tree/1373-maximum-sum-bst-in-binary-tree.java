b49df8220b592cc486b50929616689322f170450
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-05 22:55:30 +0530

    Time: 7 ms (51.28%), Space: 63.8 MB (56.42%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,33 @@
+class Pair{
+    int max;
+    int min;
+    int sum;
+    boolean isBST;
+    public Pair(int max, int min, int sum, boolean isBST){
+        this.max = max;
+        this.min = min;
+        this.sum = sum;
+        this.isBST = isBST;
+    }
+}
+class Solution {
+    static int maxSum;
+    public int maxSumBST(TreeNode root) {
+        maxSum=0;
+        Validate(root);
+        return maxSum;
+    }
+    public Pair Validate(TreeNode root){
+        if(root == null)return new Pair(Integer.MIN_VALUE, Integer.MAX_VALUE,0,true);
+        Pair lst = Validate(root.left);
+        Pair rst = Validate(root.right);
+        int max = Math.max(root.val, Math.max(lst.max,rst.max));
+        int min = Math.min(root.val, Math.min(lst.min,rst.min));
+        int size = root.val + lst.sum + rst.sum;
+        boolean isTrue = lst.isBST && rst.isBST && (lst.max < root.val) && (rst.min > root.val);
+        if(isTrue){
+            maxSum = Math.max(maxSum, size);
+        }
+        return new Pair(max, min, size, isTrue);
+    }
+}
