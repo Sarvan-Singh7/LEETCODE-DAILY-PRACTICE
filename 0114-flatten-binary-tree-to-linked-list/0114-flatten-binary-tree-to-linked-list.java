3eaad96ecd5c7022e46933feaeb7de7b246b0e0d
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-07 14:34:28 +0530

    Time: 0 ms (100%), Space: 43.9 MB (77.32%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,21 +1,44 @@
+/////suing morriss Traversal O(1) SC
 class Solution {   //==Simply Convert it to Skewed Binary tree so that it will be possibel inPlace
     public void flatten(TreeNode root) {
        if(root == null)return;
-       TreeNode lst = root.left;
-       TreeNode rst = root.right;
-       root.left = null;
-       flatten(lst);
-       flatten(rst);
-       root.right = lst;
-       TreeNode last = root;
-       while(last.right != null){
-        last = last.right;
+       TreeNode current = root;
+       while(current != null){
+        //if have pred
+        if(current.left != null){
+            TreeNode pred = current.left;
+            while(pred.right != null)pred = pred.right;
+
+            pred.right = current.right;
+            current.right = current.left;
+            current.left = null;
+        }
+        current = current.right;////execute in all wheater pred exists or not
        }
-       last.right = rst;
 
     }
     
 }
+
+
+// class Solution {   //==Simply Convert it to Skewed Binary tree so that it will be possibel inPlace
+//     public void flatten(TreeNode root) {
+//        if(root == null)return;
+//        TreeNode lst = root.left;
+//        TreeNode rst = root.right;
+//        root.left = null;
+//        flatten(lst);
+//        flatten(rst);
+//        root.right = lst;
+//        TreeNode last = root;
+//        while(last.right != null){
+//         last = last.right;
+//        }
+//        last.right = rst;
+
+//     }
+    
+// }
 // class Solution {   //==Simply Convert it to Skewed Binary tree so that it will be possibel inPlace
 //     public void flatten(TreeNode root) {
 //         ArrayList<TreeNode> arr = new ArrayList<>();
