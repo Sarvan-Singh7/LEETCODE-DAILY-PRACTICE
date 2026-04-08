a0445618495b2376b62b8f423aac65a308aed0f7
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-08 21:53:30 +0530

    Time: 1 ms (99.98%), Space: 47.2 MB (9.99%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,4 +1,6 @@
 class Solution {
+    ///IF SAME NUMBER XOR WITH SAME NUMBER OF IT SO ANSWER =0
+    //IF DIFFERENT SO ANSWER = 1;
     public int singleNumber(int[] nums) {
         int n=nums.length;
         int answer=0;
