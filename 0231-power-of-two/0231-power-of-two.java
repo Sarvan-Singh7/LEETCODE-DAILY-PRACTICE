1113ab3ff9aaecf65f60d8a4569b7bac41a3f6bb
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-09 11:33:59 +0530

    Time: 1 ms (96.57%), Space: 42.5 MB (79.4%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,24 @@
+
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        int count =0;
-        for(int i=0;i<31;i++){
-            if(n%2 == 1){
-                count++;
-            }
-            n=n>>1;
-        }
-        return count ==1;
+        if(n==0)return false;   // if n=0 so not power of 2;
+        if(n==1)return true;    ///on diving if at any point it equals 1 so return true as 1 = 2 raised to power 0
+        if(n%2 ==1)return false;   //odd number so return flase
+        return isPowerOfTwo(n/2);
     }
 }
+
+
+
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         int count =0;
+//         for(int i=0;i<31;i++){
+//             if(n%2 == 1){
+//                 count++;
+//             }
+//             n=n>>1;
+//         }
+//         return count ==1;
+//     }
+// }
