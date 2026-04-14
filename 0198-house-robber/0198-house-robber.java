8e410708183f5c1ce6ad3cfc300e92edb82593af
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-14 15:38:24 +0530

    Time: 0 ms (100%), Space: 42.6 MB (89.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,32 @@
-  class Solution {  ///will use extra space or an extra array to compute it
+//   class Solution {  ///will use extra space or an extra array to compute it
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         if(n<2)return nums[0];
+//         int dp[] = new int[n];
+//         dp[0] = nums[0];
+//         dp[1] = Math.max(nums[0], nums[1]);
+
+//         for(int i=2;i<n;i++){
+//             dp[i] = Math.max(dp[i-2] + nums[i] , dp[i-1]);
+//         }
+//         return dp[n-1];
+//     }
+// }
+  
+ /////Without Extra Space it will be like 
+  
+class Solution {  ///will use NO space or NO extra array to compute it
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n<2)return nums[0];
-        int dp[] = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
+        if(n==1)return nums[0];
+        int prev1 = nums[0];
+        int prev2 = Math.max(nums[1], nums[0]);
         for(int i=2;i<n;i++){
-            dp[i] = Math.max(dp[i-2] + nums[i] , dp[i-1]);
+            int current = Math.max(nums[i] + prev1, prev2);
+            prev1 = prev2;
+            prev2 = current;
         }
-        return dp[n-1];
+        return prev2;
     }
 }
   
