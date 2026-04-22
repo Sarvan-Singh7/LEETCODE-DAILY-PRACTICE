caf8c41be7df695be47ab1c4d2afef6a910aed82
SARVAN SINGH <thakurshravan164@gmail.com>
2026-04-22 16:45:10 +0530

    Time: 1 ms (97.79%), Space: 47 MB (58.59%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -4,17 +4,17 @@ class Solution {
         int n = intervals.length;
         List<int []> arr = new ArrayList<>();
         while(i<n && intervals[i][1] < newInterval[0]){
-            arr.add(intervals[i]);   //add existing one
+            arr.add(intervals[i]);   //add existing one which are not part of new interval
             i++;
         }
-        while(i<n && intervals[i][0] <= newInterval[1]){
+        while(i<n && intervals[i][0] <= newInterval[1]){  //if overlapping part came between two so choose minimum as newInterval first index
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
         arr.add(new int[]{newInterval[0], newInterval[1]});
 
-        while(i<n){
+        while(i<n){                     ///if any left so just simply add it
             arr.add(intervals[i]);
             i++;
         }
@@ -22,3 +22,8 @@ class Solution {
         return arr.toArray(new int[arr.size()][]);
     }
 }
+
+
+//in Conclusion = Problem divided into 3 parts === 1. find intervals which are not part of overlapping part
+//                                                 2. find minimum and maximum from intervals part of overlapping and insert after loop
+//                                                 3. just insert remaining element if exists
