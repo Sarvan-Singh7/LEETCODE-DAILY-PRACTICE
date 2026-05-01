0b54ed0915d2efe5cea0d009fe5b349bf4f9c516
SARVAN SINGH <thakurshravan164@gmail.com>
2026-05-01 09:53:53 +0530

    Time: 49 ms (61.46%), Space: 50.1 MB (54.5%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -18,29 +18,55 @@
 
 // WE HAVE TO FIND NUMBERS OF DIFFERENT INTEGERS IN THAT ARRAY WHOSE COUNT EQUALS K
 // SO DO CALCULATE FOR [NUMBER OF INTEGERS SIZE <= K ] - [NUMBER OF INTEGERS SIZE <= K-1] SO ANSWER == [INTEGERS EQUAL K]
+// class Solution {
+//     //function calculate less than equals k
+//     public static int Check(int[] nums, int k){
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int count=0;
+//         int left =0;
+//         for(int right =0;right < nums.length; right++){
+//             map.put(nums[right], map.getOrDefault(nums[right], 0) +1);
+//             while(map.size() > k){
+//                 map.put(nums[left], map.get(nums[left]) -1);
+//                 if(map.get(nums[left]) == 0)map.remove(nums[left]);
+//                 left++;
+//             }
+//             count = count + (right - left +1);
+//         }
+//         return count;
+//     }
+//     public int subarraysWithKDistinct(int[] nums, int k) {
+//     //check we calculate for gte k - gte k-1 so resultant is equalt to k wale
+//         int answer = Check(nums, k) - Check(nums, k-1);
+//         return answer;
+//     }
+// }
+
+
 class Solution {
-    //function calculate less than equals k
-    public static int Check(int[] nums, int k){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int count=0;
-        int left =0;
-        for(int right =0;right < nums.length; right++){
-            map.put(nums[right], map.getOrDefault(nums[right], 0) +1);
-            while(map.size() > k){
-                map.put(nums[left], map.get(nums[left]) -1);
-                if(map.get(nums[left]) == 0)map.remove(nums[left]);
-                left++;
+   ///write a function to calculate less than EQUAL TO then k
+   public static int check(int[] nums, int k){
+      if(k < 0)return 0;
+      int left=0,count=0;
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for(int right =0;right < nums.length; right++){
+        map.put(nums[right] , map.getOrDefault(nums[right], 0)+1);
+        while(map.size() > k){
+            map.put(nums[left], map.get(nums[left])-1);  ///-1 bahar likhna hai
+            if(map.get(nums[left]) == 0){
+                map.remove(nums[left]);
             }
-            count = count + (right - left +1);
+            left++;
         }
-        return count;
-    }
+        count = count + (right - left +1);
+      }
+      return count;
+   }
     public int subarraysWithKDistinct(int[] nums, int k) {
-    //check we calculate for gte k - gte k-1 so resultant is equalt to k wale
-        int answer = Check(nums, k) - Check(nums, k-1);
-        return answer;
+           int answer = check(nums, k) - check(nums, k-1);
+           return answer;
     }
-}
+    }
               
       
       
