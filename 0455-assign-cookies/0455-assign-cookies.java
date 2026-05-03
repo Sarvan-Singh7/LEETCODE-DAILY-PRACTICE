b9137de3309964d35838c7b33e2bbc89d29ea13b
SARVAN SINGH <thakurshravan164@gmail.com>
2026-05-03 10:08:38 +0530

    Time: 15 ms (64.77%), Space: 51.4 MB (55.62%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,14 +1,36 @@
+// class Solution {
+//     public int findContentChildren(int[] g, int[] s) {
+//         Arrays.sort(g);
+//         Arrays.sort(s);
+//         int left = 0, right =0;
+//         while(left < s.length && right < g.length){   //do for both condition otherwise error
+//             if(s[left] >= g[right]){
+//                 right++;
+//                 left++;
+//             }
+//             else if(s[left] < g[right]){
+//                 left++;
+//             }
+//         }
+//         return right;
+//     }
+// }
+
+//O(NlogN+MlogM)   == TC
+//O(1) == SC
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
         int left = 0, right =0;
         while(left < s.length && right < g.length){
-            if(s[left] >= g[right]){
-                right++;
+            if(g[right] <= s[left]){
                 left++;
+                right++;
+
             }
-            else if(s[left] < g[right]){
+            else{
                 left++;
             }
         }
