9437ea4d0dfbe59085e09b21fe1457865adb8ca3
SARVAN SINGH <thakurshravan164@gmail.com>
2026-05-12 20:37:22 +0530

    Time: 7 ms (98.83%), Space: 46.3 MB (84.16%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,81 +1,18 @@
-class Solution {   //O(n) as good code not helper function needed in it 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null)return null;
-        if(root.val == p.val || root.val == q.val)return  root;
-        TreeNode l = lowestCommonAncestor(root.left, p, q);
-        TreeNode r =lowestCommonAncestor(root.right, p , q);
-        if(l != null && r != null)return root;  //means dono side value mil gayi so simply return the node as it is answer
-        return (l!= null) ? l : r;
-    }  
+        if(root == null || root == p || root == q)return root;
+        TreeNode left =  lowestCommonAncestor(root.left, p, q);
+        TreeNode right =  lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null)return root;
+        return (left != null)?left:right;
+    }
 }
-
-
-
-//step 1 = find in which left subtree  p located and vica versa for q.
-
-//step 2 = if else ladder
-          //->if(p lies in left subtree from current root and q not in right subtree)so now please search in leftsub tree so left++;
-          //->if-else(q lies in right subtree but p not in left sub tree so )move right
-          //->else{means that p in left sub tree and q in right sub tree }so certain that current root is the ancestor in common
-
-//step 3 = just make a check function that return boolean result
-
-// class Solution {   //nearabout o(n) but in worst case it is O(nsquare)
-//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//         if(root == null)return null;
-//         if(root.val == p.val || root.val == q.val)return root; //if root ==p or q means that it is lca
-//         boolean pInLeft = check(root.left,p.val);
-//         boolean qInRight = check(root.right,q.val);
-//         if(pInLeft && !qInRight){
-//             return lowestCommonAncestor(root.left,p,q);
-//         }else if(!pInLeft && qInRight){
-//             return lowestCommonAncestor(root.right,p,q);
-//         }else{
-//             return root;
-//         }
-//     }
-//     public boolean check(TreeNode root,int val ){
-//         if(root == null)return false;
-//         if(root.val == val)return true;
-//         return check(root.left, val) || check(root.right, val);
-//     }   
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {   //nearabout o(n)
-//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//         if(root == null)return null;
-//         if(root.val == p.val || root.val == q.val)return root;   //if at any point we are at point when root becomes p q o finded
-//         boolean pLiesInLeft = check(root.left,p);
-//         boolean qLiesInRight = check(root.right,q);  //see we are not checking in rightsubtree 
-//         if(pLiesInLeft && !qLiesInRight){           //if both in left then defanately find in next iteration leftwards
-//             return lowestCommonAncestor(root.left, p, q);
-//         }
-//         else if(!pLiesInLeft && qLiesInRight){   /////if in rightsub tree
-//             return lowestCommonAncestor(root.right, p, q);
-//         }
-//         else{
-//             return root;   //executes when p in left and q in right so LCA is root 
-//         }
-//     }
-
-
-
-//     public static boolean check(TreeNode root, TreeNode val){
-//         if(root == null)return false;
-//         if(root.val == val.val)return true;
-//         return check(root.left, val) || check(root.right, val);
-//     }
-// }
