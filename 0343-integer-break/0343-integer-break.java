54170c1f745b53feac98da6cb7a56d607dab197a
SARVAN SINGH <thakurshravan164@gmail.com>
2026-05-13 23:50:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (74.4%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,13 +1,11 @@
 class Solution {
     public int integerBreak(int n) {
-        if(n <=3) return n-1;
-
-        int result = 1;
+        int result =1;
+        if(n<=3)return n-1;
         while(n > 4){
-            result *= 3;
-            n = n-3;
+            result *= 3;   //multiply karna hai ji
+            n= n-3;
         }
-        
         return result *n;
     }
 }
