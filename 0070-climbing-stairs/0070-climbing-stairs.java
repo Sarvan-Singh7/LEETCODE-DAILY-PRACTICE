44350b0d51af86c0b85170bfe7dba8c87616111e
SARVAN SINGH <thakurshravan164@gmail.com>
2026-06-08 13:38:30 +0530

    Time: 0 ms (100%), Space: 42 MB (71.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,9 +1,19 @@
 class Solution {
     public int climbStairs(int n) {
         //Memoization solution 
-        int arr[] = new int[n+1];
-        Arrays.fill(arr, -1);
-        return helper(n, arr);
+        // int arr[] = new int[n+1];
+        // Arrays.fill(arr, -1);
+        // return helper(n, arr);
+
+        ///Tabulation Solution
+        if(n <= 2)return n;
+        int prev =1,prev2 = 2;
+        for(int i=2;i<n;i++){
+            int current = prev + prev2;
+            prev= prev2;
+            prev2 = current;
+        }
+        return prev2;
 
     }
     public static int helper(int n , int arr[]){
