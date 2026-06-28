65d7d531a671380a3e51d2b03415c8d0707986a9
SARVAN SINGH <thakurshravan164@gmail.com>
2026-06-28 14:14:06 +0530

    Time: 14 ms (38.64%), Space: 57.3 MB (16.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,16 +1,35 @@
 class Solution {
-    public static int f(int []coins, int amount, int index, int dp[][]){
-        if(index ==0){
-            if(amount % coins[0] ==0)return 1;
-            else return 0;
+    // public static int f(int []coins, int amount, int index, int dp[][]){  ////MEMOIZATION
+    //     if(index ==0){
+    //         if(amount % coins[0] ==0)return 1;
+    //         else return 0;
+    //     }
+    //     if(dp[index][amount] != -1)return dp[index][amount];
+    //     int notPick = 0 + f(coins, amount, index -1, dp);
+    //     int pick =0;
+    //     if(amount >= coins[index]){
+    //         pick = f(coins, amount - coins[index], index, dp);//not index -1 as we can take multiple times
+    //     }
+    //     return dp[index][amount] = pick + notPick;
+    // }
+    public static int f(int []coins, int amount, int index, int dp[][]){ ///tabulation
+        for(int amt = 0; amt <= amount;amt++){
+            if(amt % coins[0] == 0) {
+                dp[0][amt] = 1;
+            }
+            else dp[0][amt] = 0;
         }
-        if(dp[index][amount] != -1)return dp[index][amount];
-        int notPick = 0 + f(coins, amount, index -1, dp);
-        int pick =0;
-        if(amount >= coins[index]){
-            pick = f(coins, amount - coins[index], index, dp);//not index -1 as we can take multiple times
+        for(int i=1;i<coins.length; i++){
+            for(int amt = 0; amt <= amount;amt++){
+                int notPick = dp[i-1][amt];
+                int pick =0;
+                if(amt >= coins[i]){
+                    pick = dp[i][amt - coins[i]];
+                }
+                dp[i][amt] = pick + notPick;
+            }
         }
-        return dp[index][amount] = pick + notPick;
+        return dp[coins.length-1][amount];
     }
     public int change(int amount, int[] coins) {
         int n = coins.length;
