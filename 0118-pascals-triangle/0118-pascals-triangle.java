93dad6e85cd10cd550091b893023305bb95d76a6
SARVAN SINGH <thakurshravan164@gmail.com>
2026-06-30 18:47:12 +0530

    Time: 1 ms (93.75%), Space: 43.5 MB (33.15%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,17 +1,21 @@
 class Solution {
-    public List<List<Integer>> generate(int n) {
-        List<List<Integer>> answer = new ArrayList<>();  //outer arraylist creation.
-
-        for(int i=0 ; i<n; i++ ){
-            answer.add(new ArrayList<Integer>());   //inner arrayList creation
-            for(int j=0;j<=i;j++){                          //note that loop go till j<=i
-                if(j==i || j==0){ answer.get(i).add(1);}    //handling cases for 1
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> answer = new ArrayList<>();
+        for(int i=0;i<numRows; i++){
+            List<Integer> helper = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j == 0 || j == i){
+                    helper.add(1);
+                }
                 else{
-                    int val=answer.get(i-1).get(j) + answer.get(i-1).get(j-1);    //handling cASES IN WHICH WE HAVE TO ADD ANOTHER THAN 1
-                    answer.get(i).add(val);       //MAIN THING THAT HOW TO PUSH IT MEANS  ANSWER[i].push_back(val);
+                    int a = answer.get(i-1).get(j) + answer.get(i-1).get(j-1);
+                    helper.add(a);
                 }
             }
+            answer.add(helper);
         }
-        return answer; //return answer in last;
+        return answer;
+
     }
+
 }
