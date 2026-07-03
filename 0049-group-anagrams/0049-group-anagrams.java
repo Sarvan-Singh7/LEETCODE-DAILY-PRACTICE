a0c2ead1f777fa305a4af211f7fafe4e4ea5aa06
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-03 19:18:38 +0530

    Time: 22 ms (8.3%), Space: 50.9 MB (8.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,14 +1,37 @@
-class Solution {
+// class Solution { ///O(m * nlogn) TC and O(m *n)SC
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         HashMap<String, List<String>> map = new HashMap<>();
+//         for(int i=0;i<strs.length;i++){
+//             char[] arr = strs[i].toCharArray();
+//             Arrays.sort(arr);
+//             String s = new String(arr);
+//             if(!map.containsKey(s)){
+//                 map.put(s, new ArrayList<>());   //u have to declare thisbecause we cannot declare direct interface 
+//             }
+//             map.get(s).add(strs[i]);
+//         }
+//         return new ArrayList<>(map.values());
+//     }
+// }
+
+class Solution {///O(m*n) TC and same SC
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
         for(int i=0;i<strs.length;i++){
-            char[] arr = strs[i].toCharArray();
-            Arrays.sort(arr);
-            String s = new String(arr);
-            if(!map.containsKey(s)){
-                map.put(s, new ArrayList<>());   //u have to declare thisbecause we cannot declare direct interface 
+            int[] ch = new int[26];
+            for(char s : strs[i].toCharArray()){
+                ch[ s - 'a']++;  ///storing all character Frequency
             }
-            map.get(s).add(strs[i]);
+            StringBuilder sb = new StringBuilder();
+            //now we will make key based on frequewncy of each character and after each character i will insert # so that no duplicate Key formed example if aaaaaaaaaaab & abbbbbbbbbbb will be all 3 ones so duplicate so i add # after each so it becomes 11#1# and 1#11# so unique
+            for(int s : ch){
+                sb.append(s).append('#');
+            }
+            if(!map.containsKey(sb.toString())){
+                map.put(sb.toString(), new ArrayList<>());
+            }
+            
+            map.get(sb.toString()).add(strs[i]);
         }
         return new ArrayList<>(map.values());
     }
