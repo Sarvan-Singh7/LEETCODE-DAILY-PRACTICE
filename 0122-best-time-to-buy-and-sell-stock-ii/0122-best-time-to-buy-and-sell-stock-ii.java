555c98050f8d864c3823e21570f023460da5dc41
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-04 17:25:50 +0530

    Time: 2 ms (17.37%), Space: 46.3 MB (83.26%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -12,22 +12,41 @@
 // }
 
 
-class Solution {
-    public static int f(int[] prices, int index, int buy,int n, int dp[][]){
-        if(index == n)return 0;
-        if(dp[index][buy] != -1)return dp[index][buy];
-        if(buy == 0){  //means i can chooe to buy or not
-           return dp[index][buy] =  Math.max( -prices[index] + f(prices, index+1, 1, n, dp), 0 + f(prices, index+1, 0, n, dp));
-           //means we can buy and minus value added so to add + on sell and make buy variable as 1 so to no but till i make it 0 in sell
-        }
-        else{
-            return dp[index][buy] =  Math.max( prices[index] + f(prices, index+1, 0, n, dp), 0 + f(prices, index +1, 1, n, dp));
-        }
-    }
+// class Solution {   ///Memoization Code
+//     public static int f(int[] prices, int index, int buy,int n, int dp[][]){
+//         if(index == n)return 0;
+//         if(dp[index][buy] != -1)return dp[index][buy];
+//         if(buy == 0){  //means i can chooe to buy or not
+//            return dp[index][buy] =  Math.max( -prices[index] + f(prices, index+1, 1, n, dp), 0 + f(prices, index+1, 0, n, dp));
+//            //means we can buy and minus value added so to add + on sell and make buy variable as 1 so to no but till i make it 0 in sell
+//         }
+//         else{
+//             return dp[index][buy] =  Math.max( prices[index] + f(prices, index+1, 0, n, dp), 0 + f(prices, index +1, 1, n, dp));
+//         }
+//     }
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int dp[][] = new int[n][2];
+//         for(int i=0;i<n;i++)Arrays.fill(dp[i], -1);
+//         return f(prices, 0, 0, n, dp);
+//     }
+// }
+
+class Solution {   ///Tabulation Code
+    
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][] = new int[n][2];
-        for(int i=0;i<n;i++)Arrays.fill(dp[i], -1);
-        return f(prices, 0, 0, n, dp);
+        int dp[][] = new int[n+1][2];  /// n+1
+        for(int ind = n-1; ind>=0; ind--){
+            for(int buy = 0; buy <=1; buy++){
+                if(buy == 0){
+                    dp[ind][buy] = Math.max(( -prices[ind] + dp[ind +1][1]), ( 0 + dp[ind +1][0]));
+                }
+                else{
+                    dp[ind][buy] = Math.max(( +prices[ind] + dp[ind+1][0]), (0 + dp[ind +1][1]));
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
