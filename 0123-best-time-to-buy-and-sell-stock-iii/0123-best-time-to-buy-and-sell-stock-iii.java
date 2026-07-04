c575c5cc452586e9bce147b4b63df7c6bbd2a154
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-04 23:26:26 +0530

    Time: 11 ms (48.29%), Space: 75.9 MB (72.35%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -37,30 +37,62 @@
 
 
 
-class Solution {    ////TABULATION  CODE
+// class Solution {    ////TABULATION  CODE
+//     public int maxProfit(int[] prices) {
+
+//         int n = prices.length;
+//         int dp[][][] = new int[n+1][2][3];   /// cap wala size set to 3 as to cover 0 also in 3d Grid
+        
+//         ///AS IN MEMOIZATION I- O TO N-1 SO OPPOSITE HERE
+//         //AS IN RECURSION BUY = 1 TO 0 SO O TO 1 HERE
+//         //AS IN RECURSION CAP = 2 TO 0  SO O TO 2 HERE
+
+//         ///No need to handle base cases as by default all zeroes in array
+//         for(int index=n-1; index>=0;index--){
+//             for(int buy = 0; buy <=1; buy++){
+//                 for(int cap = 1; cap <=2;cap++){  //start cap from 1 as cap 0 toh base case mein hi covered
+//                     if(buy == 1){
+//                         dp[index][buy][cap] = Math.max((-prices[index] + dp[index+1][0][cap]), (0+ dp[index+1][1][cap])) ;
+//                     }
+//                     else{
+//                         dp[index][buy][cap] = Math.max((prices[index] + dp[index+1][1][cap -1]), (0+ dp[index+1][0][cap])) ;
+                    
+//                     }
+//                 }
+//             }
+//         }
+//         return dp[0][1][2]; //1 as buy last wala remaining toh karenge hi
+//     }
+// }
+
+
+
+class Solution {    ////TABULATION  CODE with space optimization
     public int maxProfit(int[] prices) {
 
         int n = prices.length;
-        int dp[][][] = new int[n+1][2][3];   /// cap wala size set to 3 as to cover 0 also in 3d Grid
-        
-        ///AS IN MEMOIZATION I- O TO N-1 SO OPPOSITE HERE
-        //AS IN RECURSION BUY = 1 TO 0 SO O TO 1 HERE
-        //AS IN RECURSION CAP = 2 TO 0  SO O TO 2 HERE
+        int ahead[][] = new int[2][3];   
+        int current[][] = new int[2][3];
 
-        ///No need to handle base cases as by default all zeroes in array
         for(int index=n-1; index>=0;index--){
             for(int buy = 0; buy <=1; buy++){
                 for(int cap = 1; cap <=2;cap++){  //start cap from 1 as cap 0 toh base case mein hi covered
                     if(buy == 1){
-                        dp[index][buy][cap] = Math.max((-prices[index] + dp[index+1][0][cap]), (0+ dp[index+1][1][cap])) ;
+                        current[buy][cap] = Math.max((-prices[index] + ahead[0][cap]), (0+ ahead[1][cap])) ;
                     }
                     else{
-                        dp[index][buy][cap] = Math.max((prices[index] + dp[index+1][1][cap -1]), (0+ dp[index+1][0][cap])) ;
+                        current[buy][cap] = Math.max((prices[index] + ahead[1][cap -1]), (0+ ahead[0][cap])) ;
                     
                     }
                 }
             }
+             // Deep copy current into ahead before next index iteration
+            for(int b = 0; b <= 1; b++) {
+                for(int c = 0; c <= 2; c++) {
+                    ahead[b][c] = current[b][c];
+                }
+            }
         }
-        return dp[0][1][2]; //1 as buy last wala remaining toh karenge hi
+        return current[1][2]; //1 as buy last wala remaining toh karenge hi
     }
 }
