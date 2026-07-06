08363965077b2b0217d27e6b5f40478b75354829
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-06 14:52:33 +0530

    Time: 37 ms (72.24%), Space: 46.2 MB (55.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -64,22 +64,44 @@
 
 
 
-class Solution {    ///TABULATION  CODE  WITH OPTIMIZATION
+// class Solution {    ///TABULATION  CODE  WITH OPTIMIZATION
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int current[] = new int[n+1];
+//         int ahead[] = new int[n+1];
+//         for(int i = n-1; i>=0; i--){
+//             for(int j=i;j>=-1;j--){   ///not i-1 as we did index shifting
+//                 int notTake = 0 + ahead[j+1];          ///as j+1 taken just because of index shifting
+//                 int take = 0;
+//                 if( j == -1 || nums[i] > nums[j]){
+//                     take = 1 + ahead[i+1];    ////i+1 of j is taken because of index shifting
+//                 }
+//                 current[j+1] = Math.max(take, notTake);
+//             }
+//             ahead = current.clone();
+//         }
+//         return current[0];
+//     }
+// }
+
+
+
+
+class Solution {    ///simple O(n^2) Approach
+
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int current[] = new int[n+1];
-        int ahead[] = new int[n+1];
-        for(int i = n-1; i>=0; i--){
-            for(int j=i;j>=-1;j--){   ///not i-1 as we did index shifting
-                int notTake = 0 + ahead[j+1];          ///as j+1 taken just because of index shifting
-                int take = 0;
-                if( j == -1 || nums[i] > nums[j]){
-                    take = 1 + ahead[i+1];    ////i+1 of j is taken because of index shifting
+        int dp[] = new int[n];
+        int maxi =0;
+        Arrays.fill(dp, 1);   /////// fill with 1 as we know that 1 is then length of any subsequence
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i],1 +  dp[j]);
                 }
-                current[j+1] = Math.max(take, notTake);
             }
-            ahead = current.clone();
+            maxi = Math.max(maxi, dp[i]);
         }
-        return current[0];
+        return maxi;
     }
 }
