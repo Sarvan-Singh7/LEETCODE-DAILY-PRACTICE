d83c5f62fd5257ea194765ffdc66629cb6b17816
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-06 11:44:52 +0530

    Time: 395 ms (8.45%), Space: 237.1 MB (12.94%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,41 @@
+// class Solution {  ///SIMPLE TRYING ALL WAYS OF IT 
+//     public static int f(int nums[], int index, int prevInd, int n){
+//         if(index == n)return 0;    //base Case
+//         ///not take and take case
+//         int notTake = 0 + f(nums, index+1, prevInd, n);
+//         int take =0;
+//         if(prevInd == -1 || nums[index] > nums[prevInd]){
+//             take = 1 + f(nums, index+1, index, n);
+//         }
+//         return Math.max(take, notTake);
+//     }
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         ///as i have to also consider previous index for comparision so need that start at -1 and will increase it as index increased
+        
+//         return  f(nums, 0, -1, n);
+//     }
+// }
+
+
+class Solution {    ////MEMOIZATION CODE
+    public static int f(int nums[], int index, int prevInd, int n, int dp[][]){
+        if(index == n)return 0;    //base Case
+        ///not take and take case
+
+        if(dp[index][prevInd + 1] != -1)return dp[index][prevInd + 1];  // see as to not take -1 s index we did prevInd +1 so(0 to n);
+        int notTake = 0 + f(nums, index+1, prevInd, n, dp);
+        int take =0;
+        if(prevInd == -1 || nums[index] > nums[prevInd]){
+            take = 1 + f(nums, index+1, index, n, dp);
+        }
+        return dp[index][prevInd+1] =  Math.max(take, notTake);
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        ///as i have to also consider previous index for comparision so need that start at -1 and will increase it as index increased
+        int dp[][] = new int[n][n+1];
+        for(int i=0;i<n;i++)Arrays.fill(dp[i], -1);
+        return  f(nums, 0, -1, n, dp);
+    }
+}
