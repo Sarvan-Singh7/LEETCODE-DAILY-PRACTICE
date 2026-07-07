0079300e1cb93ef9cf56ff39a75e1fa5167516ba
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-07 14:52:35 +0530

    Time: 7 ms (79.85%), Space: 46.2 MB (55.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,29 @@
+////BINARY SEARCH METHOD TO SOLVE IN LESS TIME COMPLEXITY IN BS METHOD SUBSEQUENCE FORMED LENGTH IS VALID LENGTH BUT SUBSEQUENCE IS INCORRECT
+
+class Solution {
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+
+        for(int i=1;i<n;i++){
+            if(nums[i] > temp.get(temp.size() -1)){
+                temp.add(nums[i]);
+            }
+            else{
+                int index = Collections.binarySearch(temp, nums[i]);
+                if(index <0) index = -(index +1);    ///index =0 not valid so make sure this
+                temp.set(index, nums[i]);
+            }
+        }
+
+        return temp.size();
+    }
+}
+
+
+
 // class Solution {  ///SIMPLE TRYING ALL WAYS OF IT 
 //     public static int f(int nums[], int index, int prevInd, int n){
 //         if(index == n)return 0;    //base Case
@@ -87,21 +113,75 @@
 
 
 
-class Solution {    ///simple O(n^2) Approach
+// class Solution {    ///simple O(n^2) Approach
 
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        int maxi =0;
-        Arrays.fill(dp, 1);   /////// fill with 1 as we know that 1 is then length of any subsequence
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i],1 +  dp[j]);
-                }
-            }
-            maxi = Math.max(maxi, dp[i]);
-        }
-        return maxi;
-    }
-}
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int dp[] = new int[n];
+//         int maxi =0;
+//         Arrays.fill(dp, 1);   /////// fill with 1 as we know that 1 is then length of any subsequence
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(nums[j] < nums[i]){
+//                     dp[i] = Math.max(dp[i],1 +  dp[j]);
+//                 }
+//             }
+//             maxi = Math.max(maxi, dp[i]);
+//         }
+//         return maxi;
+//     }
+// }
+
+
+/////HOW TO PRINT LIS (PLEASE DO IT AGIAN AS I DID COY PASTE ITS CODE)
+
+// class Solution {
+
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         if (n == 0) return 0;
+
+//         int[] dp = new int[n];
+//         int[] parent = new int[n]; // Tracks the index of the previous element
+        
+//         Arrays.fill(dp, 1);
+//         // Initialize every element pointing to itself (no parent yet)
+//         for(int i = 0; i < n; i++) {
+//             parent[i] = i; 
+//         }
+
+//         int maxi = 1;
+//         int lastIndex = 0; // Tracks the index where LIS ends
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < i; j++) {
+//                 if (nums[j] < nums[i] && 1 + dp[j] > dp[i]) {
+//                     dp[i] = 1 + dp[j];
+//                     parent[i] = j; // Record that i came after j
+//                 }
+//             }
+//             // Update global maximum and store its ending index
+//             if (dp[i] > maxi) {
+//                 maxi = dp[i];
+//                 lastIndex = i;
+//             }
+//         }
+
+//         // Reconstruct the LIS array using the parent pointers
+//         List<Integer> lis = new ArrayList<>();
+//         lis.add(nums[lastIndex]);
+
+//         while (parent[lastIndex] != lastIndex) {
+//             lastIndex = parent[lastIndex];
+//             lis.add(nums[lastIndex]);
+//         }
+
+//         // The list is built backwards, so reverse it to get chronological order
+//         Collections.reverse(lis);
+
+//         // Print the full LIS array to console
+//         System.out.println("The Longest Increasing Subsequence is: " + lis);
+
+//         return maxi; // Still returns the length as expected by LeetCode
+//     }
+// }
