9c95f245809e56d261187276e3ff2c13ed7c4973
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-13 19:20:31 +0530

    Time: 0 ms (100%), Space: 44 MB (24.5%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,48 +1,47 @@
 class Solution {
-    public boolean BinarySearch(int arr[], int target){
-        int i=0;
-        int j=arr.length -1;
-        while(i<=j){
-            int mid = i + (j-i)/2;
-            if(target == arr[mid] )return true;
-            else if(target > arr[mid])i = mid+1;
-            else  j= mid -1;
-        }
-        return false;
-    }
-    public boolean searchMatrix(int[][] matrix, int target) {//TC = O(n) + O(logn)
-        int n = matrix.length;
-        int m = matrix[0].length;
-        for(int i=0;i<n;i++){
-            if(matrix[i][0] <= target && matrix[i][m-1] >= target){
-                if(BinarySearch(matrix[i], target)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
-    // public boolean searchMatrix(int[][] matrix, int target) {   //////O(logN) TC  as Solved
+    // public boolean BinarySearch(int arr[], int target){
+    //     int i=0;
+    //     int j=arr.length -1;
+    //     while(i<=j){
+    //         int mid = i + (j-i)/2;
+    //         if(target == arr[mid] )return true;
+    //         else if(target > arr[mid])i = mid+1;
+    //         else  j= mid -1;
+    //     }
+    //     return false;
+    // }
+    // public boolean searchMatrix(int[][] matrix, int target) {//TC = O(n) + O(logn)
     //     int n = matrix.length;
     //     int m = matrix[0].length;
-    //     int startRow =0;
-    //     int endRow = n-1;
-    //     int startCol = 0;
-    //     int endCol = m-1;
-
-    //     while(startRow <= endRow && endCol >=0){
-    //         if(matrix[startRow][endCol] > target ){
-    //             endCol--;
-    //         }
-    //         else if(matrix[startRow][endCol] < target){
-    //             startRow++;
-    //         }
-    //         else if(matrix[startRow][endCol] == target){    
-    //             return true;
+    //     for(int i=0;i<n;i++){
+    //         if(matrix[i][0] <= target && matrix[i][m-1] >= target){
+    //             if(BinarySearch(matrix[i], target)){
+    //                 return true;
+    //             }
     //         }
     //     }
     //     return false;
     // }
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {   //////O(logN) TC  as Solved
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int startRow =0;
+  
+        int endCol = m-1;
+
+        while(startRow <= n-1 && endCol >=0){
+            if(matrix[startRow][endCol] > target ){
+                endCol--;
+            }
+            else if(matrix[startRow][endCol] < target){
+                startRow++;
+            }
+            else if(matrix[startRow][endCol] == target){    
+                return true;
+            }
+        }
+        return false;
+    }
 }
