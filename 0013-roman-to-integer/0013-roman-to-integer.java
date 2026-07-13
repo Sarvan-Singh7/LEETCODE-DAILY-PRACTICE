6e763e0dd14e5ac77561a91be3d45f1c503fe158
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-13 13:04:34 +0530

    Time: 4 ms (55.53%), Space: 46.7 MB (28.76%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();  //O(1) SC as Map values are minimal
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -10,13 +10,13 @@ class Solution {
         map.put('M', 1000);
         int answer=0;
         int i=0;
-        for(i=0;i<s.length() -1;i++){
+        for(i=0;i<s.length() -1;i++){  //O(N) tc
             char ch = s.charAt(i);
             char ch2 = s.charAt(i+1);
-            if(map.get(ch) < map.get(ch2)){
+            if(map.get(ch) < map.get(ch2)){   //agar agla Character ka value Bada hai so just do   minus karke dalo and i++ so that agar "IX" input hai toh simple 9 hi calculate ho , na ki 9 + 10;
                 answer += map.get(ch2)-map.get(ch);
                 i++;
-                continue;
+                
             }
             else{
                 answer += map.get(ch);
