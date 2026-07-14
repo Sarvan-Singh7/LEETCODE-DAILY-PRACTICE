d652f6b7ccbb8cbd3e73ca8f3683539ee3179131
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-14 14:47:16 +0530

    Time: 76 ms (55.2%), Space: 44.5 MB (49.03%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,17 +1,33 @@
-class Solution {
-    public static int f(ArrayList<Integer> arr, int i, int j, int dp[][]){
-        if(i>j) return 0;
-        int maxi = Integer.MIN_VALUE;
-        if(dp[i][j] != -1)return dp[i][j];
-        for(int k=i;k <= j; k++){
-            int cost = arr.get(i-1) * arr.get(k) * arr.get(j+1); ////i and j used here check here
-            int firstPart = f(arr, i, k-1, dp);
-            int secondPart = f(arr, k+1, j, dp);
-            int total = cost + firstPart + secondPart;
-            maxi = Math.max(maxi, total);
-        }
-        return dp[i][j] =  maxi;
-    }
+// class Solution {    ///Recursion + Memoization Code
+//     public static int f(ArrayList<Integer> arr, int i, int j, int dp[][]){
+//         if(i>j) return 0;
+//         int maxi = Integer.MIN_VALUE;
+//         if(dp[i][j] != -1)return dp[i][j];
+//         for(int k=i;k <= j; k++){
+//             int cost = arr.get(i-1) * arr.get(k) * arr.get(j+1); ////i and j used here check here
+//             int firstPart = f(arr, i, k-1, dp);
+//             int secondPart = f(arr, k+1, j, dp);
+//             int total = cost + firstPart + secondPart;
+//             maxi = Math.max(maxi, total);
+//         }
+//         return dp[i][j] =  maxi;
+//     }
+//     public int maxCoins(int[] nums) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         arr.add(1);
+//         for(int num : nums)arr.add(num);
+//         arr.add(1);
+//         int n = arr.size();////size of new arrayList
+//         int dp[][] = new int[n][n];
+//         for(int i=0;i<n;i++)Arrays.fill(dp[i], -1);
+//         return f(arr, 1, arr.size() - 2, dp);  ///as first and last index of arraylist nahin bheja because of prefix and suffix
+//     }
+// }
+
+
+
+
+class Solution {  ///Tabulation Code
     public int maxCoins(int[] nums) {
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(1);
@@ -19,7 +35,22 @@ class Solution {
         arr.add(1);
         int n = arr.size();////size of new arrayList
         int dp[][] = new int[n][n];
-        for(int i=0;i<n;i++)Arrays.fill(dp[i], -1);
-        return f(arr, 1, arr.size() - 2, dp);  ///as first and last index of arraylist nahin bheja because of prefix and suffix
+        
+        for(int i = n-2; i>=1; i--){
+            for(int j = i; j <= n -2 ; j++){
+                int maxi = Integer.MIN_VALUE;
+                for(int k=i; k<=j; k++){
+                    int cost = arr.get(i -1) * arr.get(k) * arr.get(j +1);
+                    int firstPart = dp[i][k-1];
+                    int secondPart = dp[k+1][j];
+                    int total = cost + firstPart + secondPart;
+                    maxi = Math.max(maxi, total);
+                }
+        
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n-2];
     }
 }
