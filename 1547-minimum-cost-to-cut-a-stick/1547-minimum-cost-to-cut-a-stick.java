8435643f09ae35ca6dafdd6dcc407d31b1f850cc
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-14 11:44:54 +0530

    Time: 21 ms (59.92%), Space: 44.6 MB (42.16%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,30 +1,61 @@
-class Solution {
-    public static int f(ArrayList<Integer> arr, int i, int j, int dp[][]){
-        if(i>j)return 0;
-        int mini = Integer.MAX_VALUE;
-        if(dp[i][j] != -1)return dp[i][j];
+// class Solution {    // memoization + Recursion Code
+//     public static int f(ArrayList<Integer> arr, int i, int j, int dp[][]){
+//         if(i>j)return 0;
+//         int mini = Integer.MAX_VALUE;
+//         if(dp[i][j] != -1)return dp[i][j];
 
-        for(int k=i; k<= j;k++){
-            ///dono ka difference lena hai as lenght needed (not take its addition)
-            int cost = arr.get(j+1) - arr.get(i-1);///dyhan se dekho as puri length of current partition ke liye
-            int firstPart = f(arr, i, k-1, dp);
-            int secondPart = f(arr, k+1, j, dp);
-            int total = cost + firstPart + secondPart;
-            mini = Math.min(mini,total);
-        }
-        return dp[i][j]  = mini;
-    }
+//         for(int k=i; k<= j;k++){
+//             ///dono ka difference lena hai as lenght needed (not take its addition)
+//             int cost = arr.get(j+1) - arr.get(i-1);///dyhan se dekho as puri length of current partition ke liye
+//             int firstPart = f(arr, i, k-1, dp);
+//             int secondPart = f(arr, k+1, j, dp);
+//             int total = cost + firstPart + secondPart;
+//             mini = Math.min(mini,total);
+//         }
+//         return dp[i][j]  = mini;
+//     }
+//     public int minCost(int n, int[] cuts) {
+        
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         arr.add(0);   ///as length starts from 0
+//         for(int c : cuts)arr.add(c);
+//         arr.add(n);   ///as length n given hai jahan tak ja sakte hain
+//         Collections.sort(arr);
+
+//         int dp[][] = new int[arr.size()][arr.size()];
+//         for(int i=0;i<arr.size();i++)Arrays.fill(dp[i], -1);
+
+//         return f(arr, 1, arr.size() - 2, dp);///1 means cuts wale array ka 0th index ka Element and third parameter is cuts ka last index wala element
+//     }
+// }
+
+
+
+
+class Solution {     ///Tabulation Code
     public int minCost(int n, int[] cuts) {
         
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(0);   ///as length starts from 0
         for(int c : cuts)arr.add(c);
         arr.add(n);   ///as length n given hai jahan tak ja sakte hain
-        Collections.sort(arr);
+        Collections.sort(arr);    //sort karna jaruri so that independent portion ban sake
 
         int dp[][] = new int[arr.size()][arr.size()];
-        for(int i=0;i<arr.size();i++)Arrays.fill(dp[i], -1);
-
-        return f(arr, 1, arr.size() - 2, dp);///1 means cuts wale array ka 0th index ka Element and third parameter is cuts ka last index wala element
+        int len = arr.size();
+        for(int i= len -2; i>=1;i-- ){
+            for(int j = i; j<=len-2; j++){///start from i   and <= len -2
+                int mini = Integer.MAX_VALUE;
+                for(int k=i; k<=j; k++){
+                    int cost = arr.get(j+1) - arr.get(i-1);
+                    int firstPart = dp[i][k-1];
+                    int secondPart = dp[k+1][j];
+                    int total = cost + firstPart + secondPart;
+                    mini = Math.min(total, mini); 
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][len-2];
     }
 }
