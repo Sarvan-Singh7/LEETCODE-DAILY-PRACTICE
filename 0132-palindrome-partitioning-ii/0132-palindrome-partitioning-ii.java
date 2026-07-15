9d02f75f01cd8963dd703c928a828e0bdadc946b
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-15 23:52:29 +0530

    Time: 2485 ms (21.03%), Space: 42.9 MB (75.03%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -36,25 +36,7 @@
 // }
 
 
-///\(\text{States } (N) \times \text{Loop } (N) \times \text{Two-Pointer Check } (O(N)) = \mathbf{O(N^3)}\).
-class Solution {   ///O(n3) TC but still works as Not StringBuilder
-    public static int f(String s, int i, int n, int dp[]){
-        //: Change base case to -1 so the last segment doesn't add an extra cut
-        if(i == n )return -1;///as 0 return because it wont add anymore
-
-        int mini = Integer.MAX_VALUE;
-        
-        if(dp[i] != -1)return dp[i];
-        for(int j=i; j<n;j++){
-            
-            if(isPalindrome(s, i, j)){
-              int cost = 1 + f(s, j+1, n, dp);////as start from new index now so j+1 also to avoid infinite loop (SUMPLE J PASSED WILL RESULT IN RUNTIME ERROR)
-              mini = Math.min(mini, cost);  
-            }
-            
-        }
-        return dp[i] = mini;
-    }
+class Solution {   ///O(n2) TC 
     public static boolean isPalindrome(String s, int i, int j){
         
         while(i<j){
@@ -67,9 +49,22 @@ class Solution {   ///O(n3) TC but still works as Not StringBuilder
         return true;
     }
     public int minCut(String s) {
+
         int n = s.length();
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return f(s, 0, n, dp);        
+        if(n==1)return 0;
+        int dp[] = new int[n+1];
+        // Base case initialization: f(s, n) returns -1-------------------------------------
+        dp[n] = -1;
+        for(int i=n-1; i>=0;i--){
+            int mini =Integer.MAX_VALUE;
+            for(int j=i; j<n;j++){
+                if(isPalindrome(s, i, j)){
+                    int cost = 1 +  dp[j+1]; ///j+1 used so dp size increased
+                    mini = Math.min(mini, cost);  
+                 }
+            }
+            dp[i] = mini;
+        }  
+        return dp[0];     
     }
 }
