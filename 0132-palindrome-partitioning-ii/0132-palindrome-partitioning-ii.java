6b684f9d4c5ef3fc8dbb860e95171a602448cad9
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-15 23:17:45 +0530

    Time: 2479 ms (21.95%), Space: 42.5 MB (93.43%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,75 @@
+// class Solution {  ////TLE as Goes in O(N Cube) TC;
+//     public static int f(String s, int i, int n, int dp[]){
+//         //: Change base case to -1 so the last segment doesn't add an extra cut
+//         if(i == n)return -1;///as -1 return because it wont add anymore
+
+//         int mini = Integer.MAX_VALUE;
+//         StringBuilder sb = new StringBuilder();
+//         if(dp[i] != -1)return dp[i];
+//         for(int j=i; j<n;j++){
+//             sb.append(s.charAt(j));
+//             if(isPalindrome(sb)){
+//               int cost = 1 + f(s, j+1, n, dp);////as start from new index now so j+1 also to avoid infinite loop (SUMPLE J PASSED WILL RESULT IN RUNTIME ERROR)
+//               mini = Math.min(mini, cost);  
+//             }
+            
+//         }
+//         return dp[i] = mini;
+//     }
+//     public static boolean isPalindrome(StringBuilder sb){
+//         int i=0, j=sb.length() -1;
+//         while(i<j){
+//             if(sb.charAt(i) != sb.charAt(j)){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+//     public int minCut(String s) {
+//         int n = s.length();
+//         int dp[] = new int[n];
+//         Arrays.fill(dp, -1);
+//        return f(s, 0, n, dp); 
+//     }
+// }
+
+
+
+class Solution {
+    public static int f(String s, int i, int n, int dp[]){
+        //: Change base case to -1 so the last segment doesn't add an extra cut
+        if(i == n )return -1;///as -1 return because it wont add anymore
+
+        int mini = Integer.MAX_VALUE;
+        
+        if(dp[i] != -1)return dp[i];
+        for(int j=i; j<n;j++){
+            
+            if(isPalindrome(s, i, j)){
+              int cost = 1 + f(s, j+1, n, dp);////as start from new index now so j+1 also to avoid infinite loop (SUMPLE J PASSED WILL RESULT IN RUNTIME ERROR)
+              mini = Math.min(mini, cost);  
+            }
+            
+        }
+        return dp[i] = mini;
+    }
+    public static boolean isPalindrome(String s, int i, int j){
+        
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public int minCut(String s) {
+        int n = s.length();
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        return f(s, 0, n, dp);        
+    }
+}
