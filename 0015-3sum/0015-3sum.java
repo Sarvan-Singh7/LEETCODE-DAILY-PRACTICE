bb4a75e9b0f79082a428fa30ed42a532e6871ce5
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-18 16:17:59 +0530

    Time: 37 ms (24.52%), Space: 60.2 MB (14.14%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,81 +1,55 @@
-class Solution {  //NEED UNIQUE TRIPLESTS AND IN THAT ONE TRIPLET NO ELEMENT FORM SAME INDEX NOT BE TAKEN TWICE  O(nSquare)
-    public List<List<Integer>> threeSum(int[] nums) {  //== o(N2) but still not work proper
-        int n= nums.length;
-        Arrays.sort(nums);   //upar hi sort kar liya so no neeed to sort in between loops.
-        List<List<Integer>> answer = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(i >0 && nums[i] == nums[i-1]){  ///so that we do not take first element twic
-                continue;
-            }
-            int j=i+1;
-            int k = n-1;
-            while(j<k){
-                int sum =nums[i] + nums[j] + nums[k];
-                if(sum <0){j++;}
-                else if(sum >0){k--;}
-                else{    //means we got uniqu triplet where sum =0 and we do not nee to sort as full array is sorted above
-                    List<Integer> helper = new ArrayList<>();
-                    helper.add(nums[i]);helper.add(nums[j]);helper.add(nums[k]);
-                    answer.add(helper);
-                    j++;       ///when finded pair then shrink from both ends so that not duplicate in another triplet
-                    k--;
-                    while(j<k && nums[j] == nums[j-1]){j++;}///j<k here and on below means on both are important
-                    while(j<k && nums[k] == nums[k+1]){k--;}//both while loop so that we do not take j th and k th elementtwice
-
-                }
-            }
-        }
-        return answer;
-        //compare with k+1  
-    }
-}
-
-
-
-// class Solution {  //NEED UNIQUE TRIPLESTS AND IN THAT ONE TRIPLET NO ELEMENT FORM SAME INDEX NOT BE TAKEN TWICE  O(nSquare)
-//     public List<List<Integer>> threeSum(int[] nums) {  //== o(N2) but still not work proper
-//         int n= nums.length;
-//         Set<List<Integer>> set = new HashSet<>();  ///set so that no  1D duplicate arraylist
-//         for(int i=0;i<n;i++){
-//             Set<Integer> helperSet = new HashSet<>();   //so that no duplicate elementsin one single triplet
-//             for(int j=i+1;j<n;j++){
-//                 int third = -(nums[i] + nums[j]);
-//                 if(helperSet.contains(third)){
-//                   List<Integer> triplet = Arrays.asList(nums[i], third, nums[j]);
-//                   Collections.sort(triplet);
-//                   set.add(triplet);
-//                 }
-
-//                 helperSet.add(nums[j]);
-//             }
-//         }
-//         return new ArrayList<>(set);
-        
-//     }
-// } 
-
-   
-
-
-// class Solution {  //NEED UNIQUE TRIPLESTS AND IN THAT ONE TRIPLET NO ELEMENT FORM SAME INDEX NOT BE TAKEN TWICE
-//     public List<List<Integer>> threeSum(int[] nums) {  //== o(N3)
-//         int n= nums.length;
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
 //         List<List<Integer>> arr = new ArrayList<>();
+//         int n = nums.length;
 //         for(int i=0;i<n;i++){
+            
 //             for(int j=i+1;j<n;j++){
 //                 for(int k=j+1;k<n;k++){
 //                     if(nums[i] + nums[j] + nums[k] == 0){
-//                         List<Integer> helper = new ArrayList<>();//below SORTING TAKES CONSTANT TIME AS 3 ELEMENTS ONLY
+//                         List<Integer> helper = new ArrayList<>();
 //                         helper.add(nums[i]);helper.add(nums[j]);helper.add(nums[k]);
-//                         Collections.sort(helper);  //sort isliye kar liya ki easy check ho jaye ki same element wala pehle se present toh nahin
-//                         if(!arr.contains(helper)){  //for unique triplets
-//                             arr.add(helper);
-//                         }
-                        
+//                         Collections.sort(helper);
+//                         if(!arr.contains(helper)) arr.add(helper);
 //                     }
 //                 }
 //             }
 //         }
 //         return arr;
 //     }
-// } 
+// }
+
+
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> arr = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i <= n -2;i++){    //end is n-2 as we will also acces some indices like k+1
+            if( i>0 && nums[i] == nums[i-1] ){
+                continue;
+            }
+            int j=i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    ArrayList<Integer> helper = new ArrayList<>();
+                    helper.add(nums[i]); helper.add(nums[j]); helper.add(nums[k]);
+                    arr.add(helper);
+                    j++;
+                    k--;
+                    while(nums[j] == nums[j-1] && j<k) j++;
+                    while(nums[k] == nums[k+1] && j< k)k--;
+                }
+                else if (sum <0) j++;
+                else if(sum > 0)k--;
+            }
+
+        }
+        return arr;
+       
+    }
+}
