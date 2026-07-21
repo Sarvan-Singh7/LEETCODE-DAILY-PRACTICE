13de607e6dc2314b9ecb094050eb43a88b2f9401
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-21 11:34:22 +0530

    Time: 0 ms (100%), Space: 44.9 MB (8.17%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,19 +1,31 @@
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         int n = arr.length;
+//         int maxRange = n +k;//as at max the element which will be no there is n+k  see seocnd eg 
+        
+//         for(int i=1; i<=maxRange; i++){
+//             boolean finded = false;
+//             for(int j = 0; j< n ; j++){
+//                 if(i == arr[j]){
+//                     finded = true;
+//                     break;
+//                 }
+//             }
+//             if(finded == false)k--;
+//             if(k == 0)return i;
+//         }
+//         return -1;
+//     }
+// }
+
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         int n = arr.length;
-        int maxRange = n +k;//as at max the element which will be no there is n+k  see seocnd eg 
-
-        for(int i=1; i<=maxRange; i++){
-            boolean finded = false;
-            for(int j = 0; j< n ; j++){
-                if(i == arr[j]){
-                    finded = true;
-                    break;
-                }
-            }
-            if(finded == false)k--;
-            if(k == 0)return i;
+        for(int i=0;i<n;i++){
+            if(arr[i] <= k)k++;
+            else break;
         }
-        return -1;
+        return k;
     }
 }
