c85a35a7ed81cb610c14563e3cca07c9e4c0ce9e
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-21 11:09:23 +0530

    Time: 8 ms (4.97%), Space: 44.5 MB (74.22%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int maxRange = n +k;//as at max the element which will be no there is n+k  see seocnd eg 
+
+        for(int i=1; i<=maxRange; i++){
+            boolean finded = false;
+            for(int j = 0; j< n ; j++){
+                if(i == arr[j]){
+                    finded = true;
+                    break;
+                }
+            }
+            if(finded == false)k--;
+            if(k == 0)return i;
+        }
+        return -1;
+    }
+}
