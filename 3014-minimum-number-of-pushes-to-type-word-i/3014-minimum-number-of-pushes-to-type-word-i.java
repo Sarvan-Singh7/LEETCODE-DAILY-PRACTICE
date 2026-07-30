43fc4a584f87f2283dab29f9c64722df72044afa
SARVAN SINGH <thakurshravan164@gmail.com>
2026-07-30 09:40:21 +0530

    Time: 0 ms (100%), Space: 42.5 MB (99.52%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -4,7 +4,7 @@ class Solution {
         int count =0;
         if(word.length() <= 8)return word.length();
         for(int i=0; i<n;i++){
-            count+= (i/8) + 1;
+            count+= (i/8) + 1;///only think about remnainder as agar 16 se kam so remainder 1 hi ayega so 16 tak 2 add hota rahega
         }
         return count;
 
