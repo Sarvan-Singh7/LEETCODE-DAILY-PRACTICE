77459b75995e52fa42b0b53716337548e9590caf
SARVAN SINGH <thakurshravan164@gmail.com>
2026-08-01 20:46:10 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.96%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,17 +1,20 @@
 ///INTERVAL DP--------------------------------
 class Solution {
-    public static int f(int nums[], int si, int ei){
+    public static int f(int nums[], int si, int ei, int dp[][]){
         if(si == ei)return nums[si];   ///return nums[[si]] as it will also contribute
-        int left = nums[si] - f(nums, si+1, ei); ////MINUS KIYA HAI FUNCTION KO AS PERSON1 NE GAIN KIYA NUMS[SI] KO SO - F() MEANS THAT PERSON2 TRIES TO DECREMENT THAT VALUE
+        if(dp[si][ei] != -1)return dp[si][ei];
+        int left = nums[si] - f(nums, si+1, ei, dp); ////MINUS KIYA HAI FUNCTION KO AS PERSON1 NE GAIN KIYA NUMS[SI] KO SO - F() MEANS THAT PERSON2 TRIES TO DECREMENT THAT VALUE
 
-        int right = nums[ei] - f(nums, si, ei-1);   ///HERE ALSO MINUS AS PLAYER2 ALSO DECREMENT HERE
+        int right = nums[ei] - f(nums, si, ei-1, dp);   ///HERE ALSO MINUS AS PLAYER2 ALSO DECREMENT HERE
 
-        return Math.max(left, right);
+        return dp[si][ei] = Math.max(left, right);
     }
     ///HUME YEH PATA HAI KI PLAYER 1 AND PLAYER 2 MEIN SE WINNER DENA HAI, BUT WE DO THIS IN DIFFERENT WAY IN UPPER FUNCTION WE WILL JUST CALCULATE PERSON1 SCORE AND SUBTRACT PERSON2 SCORE FROM IT AND AT LAST IF IT RETURN +VE VALUE SO IT MEANS PERSON1 WINS AS POSITIVE SO LARGER
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        return f(nums, 0, n-1) >=0 ?true:false;
+        int dp[][] = new int[n][n];
+        for(int i=0; i<n;i++)Arrays.fill(dp[i], -1);///-1 fill
+        return f(nums, 0, n-1, dp) >=0 ?true:false;
         
     }
 }
