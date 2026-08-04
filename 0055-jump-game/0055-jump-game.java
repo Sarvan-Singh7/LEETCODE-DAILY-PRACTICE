d79fd2908543f066be70c079e3443be66249d36d
SARVAN SINGH <thakurshravan164@gmail.com>
2026-08-04 18:32:25 +0530

    Time: 3 ms (20.82%), Space: 47.7 MB (92.2%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,6 +4,7 @@ class Solution {
         for(int i =0;i<nums.length;i++){
             if(maxIndex <i)return false;
             maxIndex = Math.max(maxIndex, i + nums[i]);
+            if(maxIndex == nums.length -1)return true;
         }
         return true;
     }
