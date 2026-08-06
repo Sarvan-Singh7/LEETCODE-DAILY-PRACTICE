4943b37681fb3a22ca655394585f393b259bd905
SARVAN SINGH <thakurshravan164@gmail.com>
2026-08-06 18:37:50 +0530

    Time: 46 ms (74.42%), Space: 115.6 MB (90.17%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,19 +1,163 @@
+// class Solution {
+//     public int eraseOverlapIntervals(int[][] intervals) {
+//         int count =0;
+//         int n = intervals.length;
+//         Arrays.sort(intervals, (a,b) -> a[1] - b[1] );   ///i forgot to sort accordance to end time means second index
+//         int lastEnd = intervals[0][1];
+//         for(int i=1;i<n;i++){
+//             if(lastEnd > intervals[i][0]){
+//                 count++;  //no need to update lastEnd here as this interval is to be removed so why to take its value
+                
+//             }
+//             else{
+//                 lastEnd = Math.max(lastEnd, intervals[i][1] );    ///afar ye if condition ke andar likhi so not all test case pass as we need maximum lasEnd
+//             }
+//         }
+//         return count;
+//     }
+// }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int count =0;
         int n = intervals.length;
-        Arrays.sort(intervals, (a,b) -> a[1] - b[1] );   ///i forgot to sort accordance to end time means second index
-        int lastEnd = intervals[0][1];
-        for(int i=1;i<n;i++){
-            if(lastEnd > intervals[i][0]){
-                count++;  //no need to update lastEnd here as this interval is to be removed so why to take its value
-                
+        Arrays.sort(intervals, (a,b) -> a[1] - b[1]);
+        System.out.println(intervals);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        int count =0;
+        for(int i=1; i<n;i++){
+            if(end <= intervals[i][0]){
+                end = intervals[i][1];
             }
             else{
-                lastEnd = Math.max(lastEnd, intervals[i][1] );    ///afar ye if condition ke andar likhi so not all test case pass as we need maximum lasEnd
+                count++;
             }
         }
         return count;
     }
 }
    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
