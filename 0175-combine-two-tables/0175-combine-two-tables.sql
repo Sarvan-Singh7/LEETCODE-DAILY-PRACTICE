e2f414646b940880384929f609ec3a4d343ba20a
SARVAN SINGH <thakurshravan164@gmail.com>
2026-08-13 20:37:21 +0530

    Time: 420 ms (70.35%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select Person.firstName , Person.lastName , Address.city , Address.state from Person left join Address ON Person.personId = Address.personId;
+
+select firstName, lastName, city, state from Person left join Address ON Person.personId = Address.personId;
