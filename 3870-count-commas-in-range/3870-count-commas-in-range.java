0d787a66b6cc547aab4211d54f7fdb735d8855ff
SARVAN SINGH <thakurshravan164@gmail.com>
2026-09-08 09:47:24 +0530

    Time: 4 ms (16.64%), Space: 42.7 MB (22.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,9 +3,10 @@ class Solution {
         int commas =0;
         if(n<=999)return 0;
         for(int i=1000; i<=n;i++){
-            if(i <=9999)commas++;
-            else if(i<=99999 && i>9999)commas+=1;
-            else if(i<=999999 && i>99999)commas+=1;
+            commas++;
+            // if(i <=9999)commas++;
+            // else if(i<=99999 && i>9999)commas+=1;
+            // else if(i<=999999 && i>99999)commas+=1;
         }
         return commas;
     }
